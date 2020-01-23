a8ec8f0259cf35b8fa6f67b49977cad1ebaefc4b
C Shapiro <shapiroc@chromium.org>
2020-01-23 13:33:19 -0600

    config: Create bundle of config
    This is the atomic unit of config created by a given git repo (that
    doesn't necessarily know about other repos).
    
    This will then eventually be stitched together into a single config
    bundle.
    
    BUG=None
    TEST=None
    
    Change-Id: Ic1ae45c1fbf11c3a57f1809d47bb25e3846caa8f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2017164
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: David Burger <dburger@chromium.org>

--- generate.sh
+++ generate.sh
@@ -27,6 +27,7 @@ PATH="${cipd_root}:${PATH}"
 protoc -I../../ --descriptor_set_out=proto/descpb.bin \
   --include_imports \
   src/config/api/component.proto \
+  src/config/api/config_bundle.proto \
   src/config/api/design.proto \
   src/config/api/device_brand.proto \
   src/config/api/partner.proto \
