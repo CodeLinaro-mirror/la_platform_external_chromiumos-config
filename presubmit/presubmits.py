62bae9cb7578843fa337f3bb344cbfd5800d043d
David Burger <dburger@chromium.org>
2020-01-28 11:45:01 -0700

    Start a shared presubmits module for using throughout boxster repos.
    TEST=using this in conjunction with src/project/volteer/volteer repo
    BUG=chromium:1043997
    
    Change-Id: I5f02e7dfa69d45053d8212408bc6f5fdb79701c4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2025610
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: David Burger <dburger@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>

---
+++ presubmit/presubmits.py
@@ -0,0 +1,57 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Collection of helper functions that can be used to compose presubmit
+# checks. For cross repo usage import and compose into presubmit hooks
+# as follows:
+#
+#   import sys
+#   sys.path.insert(1, 'config/presubmit')
+#
+#   import presubmits
+#
+#   def CheckChangeOnUpload(input_api, output_api):
+#     results = []
+#     results.extend(presubmits.CheckGenerated(input_api, output_api))
+#     # ... other checks
+#     return results
+#
+# Note that this expects a config symlink to exist that points at this repo.
+#
+# For more details on the depot tools provided presubmit API see:
+# http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+
+def CheckGenerated(input_api, output_api):
+  """Runs a generate.sh script as a presubmit check.
+
+  Runs a generate.sh script as a presubmit check checking for successful
+  exit and no diff generated. Thus it expects a generate.sh to exist in
+  the root of the repo.
+
+  Args:
+    input_api: InputApi, provides information about the change.
+    output_api: OutputApi, provides the mechanism for returning a response.
+
+  Returns:
+    list of PresubmitError, or empty list if no errors.
+  """
+  results = []
+
+  if not input_api.subprocess.call(
+      './generate.sh',
+      shell=True,
+      stdout=input_api.subprocess.PIPE,
+      stderr=input_api.subprocess.PIPE):
+    msg = 'Error: generate.sh failed. Please fix and try again.'
+    results.append(output_api.PresubmitError(msg))
+  elif not input_api.subprocess.call(
+      'git diff --exit-code',
+      shell=True,
+      stdout=input_api.subprocess.PIPE,
+      stderr=input_api.subprocess.PIPE):
+    msg = ('Error: Running generate.sh produced a diff. Please '
+           'run the script, amend your changes, and try again.')
+    results.append(output_api.PresubmitError(msg))
+
+  return results
