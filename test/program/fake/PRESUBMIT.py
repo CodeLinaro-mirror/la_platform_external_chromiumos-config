c014c09d5a358babde438979d5e304d8e2649190
David Burger <dburger@chromium.org>
2020-03-11 11:59:20 -0600

    Fix presubmit for program / project fakes.
    Fix presubmit for program / project fakes and use the symlink canned
    presubmits approach.
    
    BUG=chromium:1041872
    TEST=forced config check failure on project/fake/fake
    
    Change-Id: Ie3194848d0755104c15182fcb48c5ec4ba6f7bc1
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2098127
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: David Burger <dburger@chromium.org>

--- test/program/fake/PRESUBMIT.py
+++ test/program/fake/PRESUBMIT.py
@@ -1,16 +1,1 @@
-# Copyright 2020 The Chromium OS Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-import sys
-
-# TODO(https://crbug.com/1046543): is there a better way to do this cross
-# repo import?
-sys.path.insert(1, 'config/presubmit')
-import presubmits
-
-def CheckChangeOnUpload(input_api, output_api):
-  return presubmits.CheckGenConfig(input_api, output_api)
-
-def CheckChangeOnCommit(input_api, output_api):
-  return presubmits.CheckGenConfig(input_api, output_api)
+config/presubmit/_PRESUBMIT.py
