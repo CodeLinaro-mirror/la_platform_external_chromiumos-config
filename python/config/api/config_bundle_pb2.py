7c92c432ae71a6faa5ee393fd6d4cda60ee831eb
C Shapiro <shapiroc@chromium.org>
2020-03-11 13:20:04 -0500

    config: Add build-target config
    Not totally sure where to put this for now.  The entire build system
    expects this data from cros_config and merging starlark with some hybrid
    yaml or something in the build system doesn't feel that great.
    
    So starting with a bit of build-target metadata for now that will be
    published by the respective programs and we'll see how it evolves.
    
    BUG=None
    TEST=None
    
    Change-Id: Ic9db697fb245d281bd06208693b643cf4f580d6e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2098129
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Commit-Queue: David Burger <dburger@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>

--- python/config/api/config_bundle_pb2.py
+++ python/config/api/config_bundle_pb2.py
@@ -17,6 +17,7 @@ from api import design_pb2 as api_dot_design__pb2
 from api import device_brand_pb2 as api_dot_device__brand__pb2
 from api import partner_pb2 as api_dot_partner__pb2
 from api import program_pb2 as api_dot_program__pb2
+from api.software import build_target_pb2 as api_dot_software_dot_build__target__pb2
 from api.software import build_config_pb2 as api_dot_software_dot_build__config__pb2
 
 
@@ -25,9 +26,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.config.api',
   syntax='proto3',
   serialized_options=_b('Z(go.chromium.org/chromiumos/config/go/api'),
-  serialized_pb=_b('\n\x17\x61pi/config_bundle.proto\x12\x15\x63hromiumos.config.api\x1a\x13\x61pi/component.proto\x1a\x10\x61pi/design.proto\x1a\x16\x61pi/device_brand.proto\x1a\x11\x61pi/partner.proto\x1a\x11\x61pi/program.proto\x1a\x1f\x61pi/software/build_config.proto\"\xf1\x02\n\x0c\x43onfigBundle\x12\x34\n\x08partners\x18\x01 \x01(\x0b\x32\".chromiumos.config.api.PartnerList\x12\x38\n\ncomponents\x18\x02 \x01(\x0b\x32$.chromiumos.config.api.ComponentList\x12\x34\n\x08programs\x18\x03 \x01(\x0b\x32\".chromiumos.config.api.ProgramList\x12\x32\n\x07\x64\x65signs\x18\x04 \x01(\x0b\x32!.chromiumos.config.api.DesignList\x12=\n\rdevice_brands\x18\x05 \x01(\x0b\x32&.chromiumos.config.api.DeviceBrandList\x12\x42\n\rbuild_configs\x18\x07 \x03(\x0b\x32+.chromiumos.config.api.software.BuildConfigJ\x04\x08\x06\x10\x07\"F\n\x10\x43onfigBundleList\x12\x32\n\x05value\x18\x01 \x03(\x0b\x32#.chromiumos.config.api.ConfigBundleB*Z(go.chromium.org/chromiumos/config/go/apib\x06proto3')
+  serialized_pb=_b('\n\x17\x61pi/config_bundle.proto\x12\x15\x63hromiumos.config.api\x1a\x13\x61pi/component.proto\x1a\x10\x61pi/design.proto\x1a\x16\x61pi/device_brand.proto\x1a\x11\x61pi/partner.proto\x1a\x11\x61pi/program.proto\x1a\x1f\x61pi/software/build_target.proto\x1a\x1f\x61pi/software/build_config.proto\"\xb5\x03\n\x0c\x43onfigBundle\x12\x34\n\x08partners\x18\x01 \x01(\x0b\x32\".chromiumos.config.api.PartnerList\x12\x38\n\ncomponents\x18\x02 \x01(\x0b\x32$.chromiumos.config.api.ComponentList\x12\x34\n\x08programs\x18\x03 \x01(\x0b\x32\".chromiumos.config.api.ProgramList\x12\x32\n\x07\x64\x65signs\x18\x04 \x01(\x0b\x32!.chromiumos.config.api.DesignList\x12=\n\rdevice_brands\x18\x05 \x01(\x0b\x32&.chromiumos.config.api.DeviceBrandList\x12\x42\n\rbuild_targets\x18\x08 \x03(\x0b\x32+.chromiumos.config.api.software.BuildTarget\x12\x42\n\rbuild_configs\x18\x07 \x03(\x0b\x32+.chromiumos.config.api.software.BuildConfigJ\x04\x08\x06\x10\x07\"F\n\x10\x43onfigBundleList\x12\x32\n\x05value\x18\x01 \x03(\x0b\x32#.chromiumos.config.api.ConfigBundleB*Z(go.chromium.org/chromiumos/config/go/apib\x06proto3')
   ,
-  dependencies=[api_dot_component__pb2.DESCRIPTOR,api_dot_design__pb2.DESCRIPTOR,api_dot_device__brand__pb2.DESCRIPTOR,api_dot_partner__pb2.DESCRIPTOR,api_dot_program__pb2.DESCRIPTOR,api_dot_software_dot_build__config__pb2.DESCRIPTOR,])
+  dependencies=[api_dot_component__pb2.DESCRIPTOR,api_dot_design__pb2.DESCRIPTOR,api_dot_device__brand__pb2.DESCRIPTOR,api_dot_partner__pb2.DESCRIPTOR,api_dot_program__pb2.DESCRIPTOR,api_dot_software_dot_build__target__pb2.DESCRIPTOR,api_dot_software_dot_build__config__pb2.DESCRIPTOR,])
 
 
 
@@ -75,7 +76,14 @@ _CONFIGBUNDLE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='build_configs', full_name='chromiumos.config.api.ConfigBundle.build_configs', index=5,
+      name='build_targets', full_name='chromiumos.config.api.ConfigBundle.build_targets', index=5,
+      number=8, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='build_configs', full_name='chromiumos.config.api.ConfigBundle.build_configs', index=6,
       number=7, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -93,8 +101,8 @@ _CONFIGBUNDLE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=185,
-  serialized_end=554,
+  serialized_start=218,
+  serialized_end=655,
 )
 
 
@@ -124,8 +132,8 @@ _CONFIGBUNDLELIST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=556,
-  serialized_end=626,
+  serialized_start=657,
+  serialized_end=727,
 )
 
 _CONFIGBUNDLE.fields_by_name['partners'].message_type = api_dot_partner__pb2._PARTNERLIST
@@ -133,6 +141,7 @@ _CONFIGBUNDLE.fields_by_name['components'].message_type = api_dot_component__pb2
 _CONFIGBUNDLE.fields_by_name['programs'].message_type = api_dot_program__pb2._PROGRAMLIST
 _CONFIGBUNDLE.fields_by_name['designs'].message_type = api_dot_design__pb2._DESIGNLIST
 _CONFIGBUNDLE.fields_by_name['device_brands'].message_type = api_dot_device__brand__pb2._DEVICEBRANDLIST
+_CONFIGBUNDLE.fields_by_name['build_targets'].message_type = api_dot_software_dot_build__target__pb2._BUILDTARGET
 _CONFIGBUNDLE.fields_by_name['build_configs'].message_type = api_dot_software_dot_build__config__pb2._BUILDCONFIG
 _CONFIGBUNDLELIST.fields_by_name['value'].message_type = _CONFIGBUNDLE
 DESCRIPTOR.message_types_by_name['ConfigBundle'] = _CONFIGBUNDLE
