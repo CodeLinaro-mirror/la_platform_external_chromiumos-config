d2166671d652ab224edb36e6178f8eb1599e0053
C Shapiro <shapiroc@chromium.org>
2020-03-12 12:55:46 -0500

    config: Adding setup to support portage installs
    This is needed so cros-config-api portage package can be installed into
    the host build environment for portage.
    
    BUG=None
    TEST=None
    
    Change-Id: I4dd7ededbd75923792af4e2206036d2dc0eb0265
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2101268
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>

---
+++ python/setup.py
@@ -0,0 +1,16 @@
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from distutils.core import setup
+
+
+setup(name='config',
+      version='1.0',
+      description='Module to access Config API python proto bindings',
+      packages=['config'],
+      package_data={'config': [
+          'api/*.py',
+          'api/software/*.py',
+          'api/software/**/*.py',
+      ]},)
