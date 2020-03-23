8e70ad03d6d0011d82b0629e1d78e6ce1ecfc540
Andrew Lamb <andrewlamb@chromium.org>
2020-03-23 08:40:44 -0600

    Run Python unittests in PRESUBMIT.
    - Similar to crrev.com/c/2083632, except without pytype,
    b/c it was causing errors on developer workstations
    (required packages not installed). Enable unittests on
    presubmit right now, will enable pytype in the future.
    
    BUG=chromium:1058035
    TEST=repo upload
    
    Change-Id: Ic2e1b9eec393793537f9d0a3644cf08f6e0c752a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2115406
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Auto-Submit: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>

---
+++ run_py_unittests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -e
+#
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Runs python unittests in a venv.
+
+# Move to this script's directory.
+cd "$(dirname "$0")"
+
+# Generate protos
+echo "Generating proto bindings..."
+./generate.sh
+
+# Create and activate venv.
+echo "Creating and activating venv..."
+/usr/bin/python3 -m venv .venv
+source .venv/bin/activate
+
+# Install requirements.
+echo "Installing required packages..."
+pip install -r requirements.txt -q
+
+# Discover and run unittests in payload_utils.
+echo "Running unittests..."
+python3 -m unittest discover -s payload_utils -p "*test.py"
+
+# Deactivate venv.
+deactivate
