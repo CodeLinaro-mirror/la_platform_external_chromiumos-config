7fd1dbe7c329bbc721738a67bdd4e93dead24f2f
David Burger <dburger@chromium.org>
2020-03-26 09:26:55 -0600

    Copy cros_config_proto_converter to config repo, use in gen_config.
    Copy the cros_config_proto_converter into the config repo and then use
    it within gen_config when we are looking at a project (not a program).
    
    This is part of the migration to checking platform json into each
    project repo instead of the current approach where the json is generated
    on the fly by the ebuild. The first steps will get this working here,
    have projects check in platform json. The last step will be to switch
    the ebuild to aggregating the platform json rather than generating it
    itself.
    
    BUG=None
    TEST=gen_config config.star in src/project/volteer/halvor (reg. project)
    TEST=gen_config config.star in src/program/zork (program)
    TEST=gen_config config.star in src/program/zork/zork (nested project)
    
    Change-Id: I4e7cf9705f9a0ee89b9c8ecc9bffe5ef6c433a9f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2123047
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>

---
+++ payload_utils/cros_config_proto_converter_unittest.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# pylint: disable=module-missing-docstring,class-missing-docstring
+
+import os
+import subprocess
+import tempfile
+import unittest
+
+import cros_config_proto_converter
+
+
+from config.test import fake_config
+
+
+THIS_DIR = os.path.dirname(__file__)
+
+PROGRAM_CONFIG_FILE = fake_config.FAKE_PROGRAM_CONFIG
+PROJECT_CONFIG_FILE = fake_config.FAKE_PROJECT_CONFIG
+
+
+def fakeConfig():
+  return cros_config_proto_converter._MergeConfigs([
+      cros_config_proto_converter._ReadConfig(PROGRAM_CONFIG_FILE),
+      cros_config_proto_converter._ReadConfig(PROJECT_CONFIG_FILE)])
+
+
+class ParseArgsTests(unittest.TestCase):
+
+  def testParseArgs(self):
+    argv = ['-c', 'config1', 'config2',
+            '-p', 'program_config',
+            '-o', 'output', ]
+    args = cros_config_proto_converter.ParseArgs(argv)
+    self.assertEqual(args.project_configs, ['config1', 'config2',])
+    self.assertEqual(args.program_config, 'program_config')
+    self.assertEqual(args.output, 'output')
+
+
+class MainTest(unittest.TestCase):
+
+  def testFullTransform(self):
+    with tempfile.TemporaryDirectory() as tempdir:
+      output_file = os.path.join(tempdir, 'output')
+      cros_config_proto_converter.Main(project_configs=[PROJECT_CONFIG_FILE],
+                                       program_config=PROGRAM_CONFIG_FILE,
+                                       output=output_file,)
+
+      expected_file = os.path.join(THIS_DIR, 'test_data/fake_project.json')
+      changed = subprocess.run(
+          ['diff', expected_file, output_file]).returncode != 0
+
+      regen_cmd = ('To regenerate the expected output, run:\n'
+                   '\tpython3 -m cros_config_host.cros_config_proto_converter '
+                   '-c %s '
+                   '-p %s '
+                   '-o %s ' % (
+                       PROJECT_CONFIG_FILE, PROGRAM_CONFIG_FILE, expected_file))
+
+      if changed:
+        print(regen_cmd)
+        self.fail('Fake project transform does not match')
+
+
+class TransformBuildConfigsTest(unittest.TestCase):
+
+  def testMissingLookups(self):
+    config = fakeConfig()
+    config.ClearField('build_targets')
+
+    with self.assertRaisesRegex(Exception, 'Failed to lookup BuildTarget'):
+      cros_config_proto_converter._TransformBuildConfigs(config)
+
+  def testEmptyDeviceBrand(self):
+    config = fakeConfig()
+    config.ClearField('device_brands')
+
+    self.assertIsNotNone(
+        cros_config_proto_converter._TransformBuildConfigs(config))
+
+  def testMissingSwConfig(self):
+    config = fakeConfig()
+    config.ClearField('software_configs')
+
+    with self.assertRaisesRegex(Exception, 'Software config is required'):
+      cros_config_proto_converter._TransformBuildConfigs(config)
+
+  def testUniqueConfigsOnly(self):
+    config = fakeConfig()
+    duplicate_config = cros_config_proto_converter._MergeConfigs(
+        [config, fakeConfig()])
+
+    with self.assertRaisesRegex(Exception, 'Multiple software configs'):
+      cros_config_proto_converter._TransformBuildConfigs(duplicate_config)
+
+
+if __name__ == '__main__':
+  unittest.main(module=__name__)
