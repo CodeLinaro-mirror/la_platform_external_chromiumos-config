f087346e5d21b653cfd17d710fd1341328789f6a
Andrew Lamb <andrewlamb@chromium.org>
2020-04-01 13:01:42 -0600

    Check go tests pass in PRESUBMIT.
    BUG=chromium:1063477
    TEST=CQ
    TEST=Break go build and upload fails
    
    Change-Id: I147cd8a828d25fb171e4f78660fcf4dd5fe45453
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2132912
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Auto-Submit: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>

---
+++ run_go_unittests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+#
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Move to this script's directory.
+script_dir="$(realpath "$(dirname "$0")")"
+cd "${script_dir}"
+
+# Get go from CIPD.
+echo "Getting go from CIPD..."
+cipd_root="${script_dir}/.cipd_bin"
+cipd ensure \
+  -log-level warning \
+  -root "${cipd_root}" \
+  -ensure-file - \
+  <<ENSURE_FILE
+infra/3pp/tools/go/\${platform} latest
+ENSURE_FILE
+
+PATH="${cipd_root}/bin:${PATH}"
+
+echo "Running unittests..."
+cd go
+go test ./...
