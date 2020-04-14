ba59a3e276bd8a3808501932c12e5e19c2cd320b
David Burger <dburger@chromium.org>
2020-04-14 12:08:08 -0600

    Add two safety prompts to setup_project.sh.
    Add two safety prompts to setup_project.sh in the obvious locations:
    
    1) before doing anything, warning googlers with internal checkouts that
    they don't need to run this script.
    2) before wiping the ${clone_src} directory in presumed repair
    situations
    
    BUG=chromium:1070415
    TEST=manually tested with checkouts on my box
    
    Change-Id: I60abf727c7847dc9e210c8d69dbd30ab3d972dfd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2149585
    Reviewed-by: Aaron Durbin <adurbin@google.com>
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: David Burger <dburger@chromium.org>

--- setup_project.sh
+++ setup_project.sh
@@ -10,6 +10,14 @@ function bail() {
   exit 1
 }
 
+function prompt_continue() {
+  read -p "${1} (y/N) " answer
+
+  if [[ "${answer^^}" != "Y" ]]; then
+    exit 0
+  fi
+}
+
 function usage() {
   echo "Usage: $0 <program> [<project>]" >&2
   echo "  for setups with a single repo for the program with" >&2
@@ -18,12 +26,13 @@ function usage() {
   exit 1
 }
 
-if [[ $# -eq 0  ]] || [[ $# -gt 2 ]]; then
-  usage
-fi
+# Exit if any command fails.
+set -e
 
-# Exit if any command fails, print commands and their arguments.
-set -ex
+prompt_continue "
+If you are a googler and are working with an internal checkout you do
+not need to run this script as you already have a full repo checkout.
+Do you want to continue running this script?"
 
 # Move to this script's directory.
 cd "$(dirname "$0")"
@@ -50,6 +59,13 @@ if [[ -d "${clone_src}" ]]; then
   # when they found they didn't have adequate permissions on a first
   # run. In this case we wipe the artifacts from the previous run and
   # try again.
+  prompt_continue "
+${clone_src} appears to already exist. If you are
+attempting to recover from a previous failed setup_project.sh attempt
+this will attempt to fix it by removing it and resyncing. If you have
+unsaved changes in ${clone_src} they will be lost.
+Do you want to continue with the removal and resync?"
+
   echo "Founding existing ${clone_src} checkout, removing."
   rm -rf "${clone_src}"
   rm -f "${symlink}"
