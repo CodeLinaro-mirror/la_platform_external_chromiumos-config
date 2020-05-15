ab994fd8a5ba923e057e92816708650f67ae8f13
Congbin Guo <guocb@google.com>
2020-05-15 13:08:18 -0700

    tls: CacheForDut API proposal
    This CL is part of the TLW API proposal for caching. Please refer [1]
    for the proposal content. (This is the initial proposal of this API.)
    
    [1]: https://docs.google.com/document/d/14yb4dmwgNIaZMfuT88AQR94PHXrErZb-w4q2mJjpfYA/edit
    
    BUG=None
    TEST=presubmit
    
    Change-Id: Ib56e29a44f4a9af878387d1821ba5337560b3a85
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2211198
    Commit-Queue: Congbin Guo <guocb@chromium.org>
    Tested-by: Congbin Guo <guocb@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Auto-Submit: Congbin Guo <guocb@chromium.org>

---
+++ go/api/test/tls/wiring_test.go
@@ -0,0 +1,84 @@
+// Copyright 2020 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package tls_test
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/golang/protobuf/ptypes"
+	rtd "go.chromium.org/chromiumos/config/go/api/test/rtd/v1"
+	"go.chromium.org/chromiumos/config/go/api/test/tls"
+	"google.golang.org/genproto/googleapis/longrunning"
+	"google.golang.org/grpc"
+)
+
+func ExampleWiringClient_CacheForDut() {
+	var invocation rtd.Invocation
+
+	tlsConfig := invocation.GetTestLabServicesConfig()
+	dutName := invocation.GetDuts()[0].GetTlsDutName()
+	gsURL := "gs://my-bucket/path/to/file"
+
+	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", tlsConfig.GetTlwAddress(), tlsConfig.GetTlwPort()))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	c := tls.NewWiringClient(conn)
+	opcli := longrunning.NewOperationsClient(conn)
+	ctx := context.Background()
+
+	req := tls.CacheForDutRequest{
+		Url:     gsURL,
+		DutName: dutName,
+	}
+	op, err := c.CacheForDut(ctx, &req)
+	if err != nil {
+		panic("RPC error")
+	}
+
+	for {
+		if op.GetDone() {
+			break
+		}
+		time.Sleep(5 * time.Second)
+		op, err = opcli.GetOperation(ctx, &longrunning.GetOperationRequest{
+			Name: op.GetName(),
+		})
+		if err != nil {
+			panic("RPC error")
+		}
+	}
+	if errStatus := op.GetError(); errStatus != nil {
+		panic("Operation error")
+	}
+	resp := &tls.CacheForDutResponse{}
+	if err := ptypes.UnmarshalAny(op.GetResponse(), resp); err != nil {
+		panic("Unmarshal response error")
+	}
+	// Handle the response in various ways.
+	_ = resp
+}
+
+func ExampleWiringClient_CacheForDut_DownloadCachedFile() {
+	var dutName string
+	var c tls.CommonClient
+	var resp tls.CacheForDutResponse // Response of CacheForDut.
+
+	ctx := context.Background()
+	result, err := c.ExecDutCommand(ctx, &tls.ExecDutCommandRequest{
+		Name:    dutName,
+		Command: "wget",
+		Args:    []string{resp.GetUrl()},
+	})
+	if err != nil {
+		panic("RPC error")
+	}
+	// Check the return code of ExecDutCommand.
+	_ = result
+}
