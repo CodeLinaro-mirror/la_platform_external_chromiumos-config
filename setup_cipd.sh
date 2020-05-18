f79b1f6c9235914f02ed670e767e4561da80dc6d
Andrew Lamb <andrewlamb@chromium.org>
2020-05-18 10:39:04 -0600

    Specify GOROOT when calling go get buildifier.
    - Needed to fix version mismatches between cipd
    go and system go.
    
    BUG=None
    TEST=config.star
    TEST=CQ
    
    Change-Id: I3f72b38748e8ff1f367573cf0e4dc2f76b3a521a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2207520
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Auto-Submit: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>

--- setup_cipd.sh
+++ setup_cipd.sh
@@ -32,5 +32,5 @@ PATH="${cipd_root}:${PATH}"
 PATH="${cipd_root}/bin:${PATH}"
 
 # Install buildifier (Starlark formatter) and add to PATH.
-go get github.com/bazelbuild/buildtools/buildifier
+GOROOT="${cipd_root}" go get github.com/bazelbuild/buildtools/buildifier
 PATH="$(go env GOPATH)/bin:${PATH}"
