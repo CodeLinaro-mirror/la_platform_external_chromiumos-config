0e9c203419254e1c6f304339a899c534c4ccbae6
Andrew Lamb <andrewlamb@chromium.org>
2020-06-08 11:14:06 -0600

    Reland "Enforce pytype on payload_utils."
    This is a reland of 88170103f57ddc08d855c6da3d56e74783a230c7
    
    Change to just run in a script instead of actually being
    enforced by PRESUBMIT. Getting this cleanly installed on
    local setups has problems, and it doesn't seem to break
    often anyway.
    
    Original change's description:
    > Enforce pytype on payload_utils.
    >
    > - Move pytype.cfg into payload_utils, to
    > make specifying dirs a bit easier.
    >
    > - wheel and upgraded setuptools is needed to
    > install Python packages cleanly.
    >
    > BUG=chromium:1092516
    > TEST=repo upload outside of chroot
    > TEST=repo upload inside chroot
    > TEST=CQ
    >
    > Change-Id: I54a5864750ab2b9a22a97f70904239e205637a33
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2235921
    > Reviewed-by: Sean McAllister <smcallis@google.com>
    > Reviewed-by: David Burger <dburger@chromium.org>
    > Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    > Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    
    BUG=chromium:1092516
    TEST=./run_pytype.sh
    
    Change-Id: I335a58ab13983af9f20a3c7559a44ad2d00dd74b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2239712
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Commit-Queue: David Burger <dburger@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Auto-Submit: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: David Burger <dburger@chromium.org>

---
+++ run_pytype.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -e
+#
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Runs pytype on payload_utils.
+
+# Create and activate venv.
+echo "Creating and activating venv..."
+/usr/bin/python3 -m venv .venv
+source .venv/bin/activate
+
+echo "Installing pytype..."
+pip install -q pytype==2020.6.1
+
+echo "Running pytype..."
+pytype --config=payload_utils/pytype.cfg
+
+deactivate
