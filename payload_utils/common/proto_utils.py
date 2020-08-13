3877af16eb240970fcbf9bd754fe51d0136704d1
Andrew Lamb <andrewlamb@chromium.org>
2020-08-13 10:26:21 -0600

    Implement PublicReplication filtering.
    - Done in apply_public_replication method, will be called by CLI
    tools in the future. Follows semantics outlined in the
    PublicReplication proto comments and FieldMask's MergeMessage.
    
    - Add testdata proto so we don't need to rely on certain cases
    appearing in real proto schemas.
    
    - Disallow PublicReplication messages appearing on top of each
    other in proto schemas.
    
    BUG=chromium:1092530
    TEST=CQ
    
    Change-Id: Ib00ef00eaac037550f440affaf1a14d5f8724918
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2354391
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Tested-by: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: Sean McAllister <smcallis@google.com>
    Reviewed-by: David Burger <dburger@chromium.org>

--- payload_utils/common/proto_utils.py
+++ payload_utils/common/proto_utils.py
@@ -8,6 +8,8 @@ from typing import Any, Dict, List, Text
 
 from google.protobuf import message as pb_message
 
+from chromiumos.config.public_replication import public_replication_pb2
+
 
 def get_all_fields(message: pb_message.Message) -> List[Any]:
   """Returns the value of each field in message.
@@ -70,3 +72,75 @@ def get_dep_order(message: pb_message.Message) -> List[Text]:
   deps = []
   dfs(get_dep_graph(message), deps.append, message.DESCRIPTOR.full_name)
   return deps
+
+
+def __apply_public_replication_internal(src: pb_message.Message,
+                                        dst: pb_message.Message,
+                                        public_replication_found: bool):
+  """Private function to do most of the work of apply_public_replication.
+
+  Allows bookkeeping information to be passed between recursive calls, along
+  with the src and dst messages.
+
+  Args:
+    src: Source message.
+    dst: Destination message to be merged into.
+    public_replication_found: True if a PublicReplication message has already
+      been found earlier in a pre-order traversal of the dependency graph. This
+      is used to detect if two PublicReplication messages appear on top of each
+      other.
+  """
+  # First, see if any of the fields on src are a PublicReplication message. If
+  # one is found, apply the field mask within it and setpublic_replication_found.
+  for field_descriptor in src.DESCRIPTOR.fields:
+    message_descriptor = field_descriptor.message_type
+    if (message_descriptor and message_descriptor.full_name
+        == public_replication_pb2.PublicReplication.DESCRIPTOR.full_name):
+      if public_replication_found:
+        raise ValueError(
+            'PublicReplication messages may not be defined on top'
+            f' of each other. Violating message: {src.DESCRIPTOR.full_name}')
+      public_replication_found = True
+
+      public_fields = getattr(src, field_descriptor.name).public_fields
+      public_fields.MergeMessage(src, dst)
+
+  # Iterate the fields of src and call __apply_public_replication_internal
+  # recursively.
+  for field_descriptor in src.DESCRIPTOR.fields:
+    if field_descriptor.type != field_descriptor.TYPE_MESSAGE:
+      continue
+
+    if field_descriptor.label == field_descriptor.LABEL_REPEATED:
+      # For repeated fields, for each message in src, create a new message in
+      # dst and call __apply_public_replication_internal.
+      for next_src in getattr(src, field_descriptor.name):
+        next_dst = getattr(dst, field_descriptor.name).add()
+        __apply_public_replication_internal(next_src, next_dst,
+                                            public_replication_found)
+    else:
+      # For non-repeated fields, get the field in src and dst and call
+      # __apply_public_replication_internal.
+      next_src = getattr(src, field_descriptor.name)
+      next_dst = getattr(dst, field_descriptor.name)
+      __apply_public_replication_internal(next_src, next_dst,
+                                          public_replication_found)
+
+
+def apply_public_replication(src: pb_message.Message, dst: pb_message.Message):
+  """Traverses src and merges fields to dst when a PublicReplication message is
+  found.
+
+  See the comment on the PublicReplication proto for a complete description of
+  the semantics of PublicReplication.
+
+  Args:
+    src: Source message.
+    dst: Destination message to be merged into.
+  """
+  if src.DESCRIPTOR.full_name != dst.DESCRIPTOR.full_name:
+    raise ValueError(
+        'src and dst must be the same message type. Got '
+        f'{src.DESCRIPTOR.full_name} and {dst.DESCRIPTOR.full_name}')
+
+  __apply_public_replication_internal(src, dst, public_replication_found=False)
--- payload_utils/common/proto_utils_test.py
+++ payload_utils/common/proto_utils_test.py
@@ -5,9 +5,20 @@
 
 import unittest
 
+from google.protobuf import field_mask_pb2
 from google.protobuf import timestamp_pb2
 
 from chromiumos.config.api.software import build_target_pb2
+
+from chromiumos.config.public_replication.public_replication_pb2 import (
+    PublicReplication)
+from chromiumos.config.public_replication.testdata.public_replication_testdata_pb2 import (
+    PublicReplicationTestdata,
+    WrapperTestdata1,
+    WrapperTestdata2,
+    WrapperTestdata3,
+)
+
 from common import proto_utils
 
 
@@ -39,3 +50,139 @@ class ProtoUtilsTest(unittest.TestCase):
             'chromiumos.config.api.software.BuildTargetId',
             'chromiumos.config.api.software.BuildTarget'
         ])
+
+  def test_apply_public_replication(self):
+    """Tests applying the PublicReplication message in the case where it is a
+    field on the src argument.
+
+    In this case, since PublicReplication is a field on
+    PublicReplicationTestdata, no recursion is needed.
+    """
+    src = PublicReplicationTestdata(
+        str1='abc',
+        str2='def',
+        public_replication=PublicReplication(
+            public_fields=field_mask_pb2.FieldMask(paths=['str1'])),
+    )
+
+    expected = PublicReplicationTestdata(str1='abc')
+
+    dst = PublicReplicationTestdata()
+    proto_utils.apply_public_replication(src, dst)
+    self.assertEqual(dst, expected)
+
+  def test_apply_public_replication_nested_field(self):
+    """Tests applying the PublicReplication message in the case where it is in
+    a nested field on the src argument.
+
+    In this case, recursion is required: src is a WrapperTestdata2, which has a
+    WrapperTestdata1, which has a PublicReplicationTestdata.
+    """
+    src = WrapperTestdata2(
+        wrapper_testdata1=WrapperTestdata1(
+            n1=1,
+            pr_testdata=PublicReplicationTestdata(
+                str1='abc',
+                str2='def',
+                public_replication=PublicReplication(
+                    public_fields=field_mask_pb2.FieldMask(paths=['str1'])),
+            )))
+
+    expected = WrapperTestdata2(
+        wrapper_testdata1=WrapperTestdata1(
+            pr_testdata=PublicReplicationTestdata(str1='abc',)))
+
+    dst = WrapperTestdata2()
+    proto_utils.apply_public_replication(src, dst)
+    self.assertEqual(dst, expected)
+
+  def test_apply_public_replication_nested_repeated_field(self):
+    """Tests applying the PublicReplication message in the case where it is in
+    a nested repeated field on the src argument.
+
+    This case is similar to test_apply_public_replication_nested_field, but the
+    PublicReplicationTestdata field is repeated. Note that the two
+    PublicReplicationTestdata messages set different public_fields; this is
+    possible, but not necessarily expected.
+    """
+    src = WrapperTestdata2(
+        wrapper_testdata1=WrapperTestdata1(
+            n1=1,
+            repeated_pr_testdata=[
+                PublicReplicationTestdata(
+                    str1='abc',
+                    str2='def',
+                    public_replication=PublicReplication(
+                        public_fields=field_mask_pb2.FieldMask(paths=['str1'])),
+                ),
+                PublicReplicationTestdata(
+                    str1='abc',
+                    str2='def',
+                    public_replication=PublicReplication(
+                        public_fields=field_mask_pb2.FieldMask(paths=['str2'])),
+                ),
+            ]))
+
+    expected = WrapperTestdata2(
+        wrapper_testdata1=WrapperTestdata1(repeated_pr_testdata=[
+            PublicReplicationTestdata(str1='abc'),
+            PublicReplicationTestdata(str2='def'),
+        ]))
+
+    dst = WrapperTestdata2()
+    proto_utils.apply_public_replication(src, dst)
+    self.assertEqual(dst, expected)
+
+  def test_apply_public_replication_stacked_messages(self):
+    """Tests that a PublicReplication message appearing on top of another in the
+    dependency tree raises an Error.
+    """
+    with self.assertRaisesRegex(
+        ValueError,
+        'PublicReplication messages may not be defined on top of each other. '
+        'Violating message: chromiumos.config.public_replication.testdata.PublicReplicationTestdata'
+    ):
+      proto_utils.apply_public_replication(WrapperTestdata3(),
+                                           WrapperTestdata3())
+
+  def test_apply_public_replication_empty_paths(self):
+    """Tests applying the PublicReplication message with empty paths.
+
+    If the FieldMask has empty paths, not fields should be replicated. Also test
+    similar cases where public_replication and public_fields are not set.
+    """
+    srcs = [
+        PublicReplicationTestdata(
+            str1='abc',
+            str2='def',
+            public_replication=PublicReplication(
+                public_fields=field_mask_pb2.FieldMask(paths=[])),
+        ),
+        PublicReplicationTestdata(
+            str1='abc',
+            str2='def',
+        ),
+        PublicReplicationTestdata(
+            str1='abc',
+            str2='def',
+            public_replication=PublicReplication(),
+        )
+    ]
+
+    expected = PublicReplicationTestdata()
+
+    for src in srcs:
+      dst = PublicReplicationTestdata()
+      proto_utils.apply_public_replication(src, dst)
+      self.assertEqual(dst, expected)
+
+  def test_apply_public_replication_different_messages(self):
+    """Tests that passing different messages to apply_public_replication raises
+    an Error.
+    """
+    with self.assertRaisesRegex(
+        ValueError, 'src and dst must be the same message type. Got '
+        'chromiumos.config.public_replication.testdata.PublicReplicationTestdata'
+        ' and chromiumos.config.public_replication.testdata.WrapperTestdata1'):
+      proto_utils.apply_public_replication(PublicReplicationTestdata(),
+                                           WrapperTestdata1())
