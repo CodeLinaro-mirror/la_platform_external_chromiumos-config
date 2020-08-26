4fa5275df6a2cc852d72d1c30724c7a1a51c14cc
Sean McAllister <smcallis@google.com>
2020-08-26 14:34:57 -0600

    Fix shellcheck errors and warnings in bin/*
    BUG=None
    TEST=gen_config
    
    Change-Id: Id17c7b2d6981b43692942368692abe68fb39e13e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2378107
    Commit-Queue: Sean McAllister <smcallis@google.com>
    Tested-by: Sean McAllister <smcallis@google.com>
    Reviewed-by: David Burger <dburger@chromium.org>
    Auto-Submit: Sean McAllister <smcallis@google.com>

--- bin/common.sh
+++ bin/common.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+
 # Copyright 2020 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 # Common bash functions used for configuration generation and transforms.
 function summarize() {
+  # shellcheck disable=SC2181
   if [[ $? -eq 0 ]]; then
     echo "Config generation step succeeded"
   else
