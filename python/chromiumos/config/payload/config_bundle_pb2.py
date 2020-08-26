f0d632779db96ae73578760b44f18093821eae59
C Shapiro <shapiroc@chromium.org>
2020-08-26 09:03:33 -0500

    Initial release milestone payload def
    This will drive collective milestone definitions/planning, which can
    then be used to drive other logistical bits, such as device-project
    planning, test planning targeted to milestone, ...
    
    Change-Id: Iee20ce1d2fa26d10a5acabc5773d8c81f801f688
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2377861
    Tested-by: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Sean Abraham <seanabraham@chromium.org>
    Commit-Queue: C Shapiro <shapiroc@chromium.org>

--- python/chromiumos/config/payload/config_bundle_pb2.py
+++ python/chromiumos/config/payload/config_bundle_pb2.py
@@ -17,6 +17,7 @@ from chromiumos.config.api import design_pb2 as chromiumos_dot_config_dot_api_do
 from chromiumos.config.api import device_brand_pb2 as chromiumos_dot_config_dot_api_dot_device__brand__pb2
 from chromiumos.config.api import partner_pb2 as chromiumos_dot_config_dot_api_dot_partner__pb2
 from chromiumos.config.api import program_pb2 as chromiumos_dot_config_dot_api_dot_program__pb2
+from chromiumos.config.api.release import milestone_pb2 as chromiumos_dot_config_dot_api_dot_release_dot_milestone__pb2
 from chromiumos.config.api.software import brand_config_pb2 as chromiumos_dot_config_dot_api_dot_software_dot_brand__config__pb2
 from chromiumos.config.api.software import build_target_pb2 as chromiumos_dot_config_dot_api_dot_software_dot_build__target__pb2
 from chromiumos.config.api.software import software_config_pb2 as chromiumos_dot_config_dot_api_dot_software_dot_software__config__pb2
@@ -28,9 +29,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.config.payload',
   syntax='proto3',
   serialized_options=_b('Z,go.chromium.org/chromiumos/config/go/payload'),
-  serialized_pb=_b('\n-chromiumos/config/payload/config_bundle.proto\x12\x19\x63hromiumos.config.payload\x1a%chromiumos/config/api/component.proto\x1a\"chromiumos/config/api/design.proto\x1a(chromiumos/config/api/device_brand.proto\x1a#chromiumos/config/api/partner.proto\x1a#chromiumos/config/api/program.proto\x1a\x31\x63hromiumos/config/api/software/brand_config.proto\x1a\x31\x63hromiumos/config/api/software/build_target.proto\x1a\x34\x63hromiumos/config/api/software/software_config.proto\x1a-chromiumos/config/api/test/plan/v1/plan.proto\"\xd3\x04\n\x0c\x43onfigBundle\x12\x34\n\x0cpartner_list\x18\x0b \x03(\x0b\x32\x1e.chromiumos.config.api.Partner\x12\x34\n\ncomponents\x18\x0c \x03(\x0b\x32 .chromiumos.config.api.Component\x12\x34\n\x0cprogram_list\x18\r \x03(\x0b\x32\x1e.chromiumos.config.api.Program\x12\x32\n\x0b\x64\x65sign_list\x18\x0e \x03(\x0b\x32\x1d.chromiumos.config.api.Design\x12=\n\x11\x64\x65vice_brand_list\x18\x0f \x03(\x0b\x32\".chromiumos.config.api.DeviceBrand\x12\x42\n\rbuild_targets\x18\x08 \x03(\x0b\x32+.chromiumos.config.api.software.BuildTarget\x12H\n\x10software_configs\x18\t \x03(\x0b\x32..chromiumos.config.api.software.SoftwareConfig\x12\x42\n\rbrand_configs\x18\n \x03(\x0b\x32+.chromiumos.config.api.software.BrandConfig\x12J\n\x0ftest_plan_specs\x18\x10 \x03(\x0b\x32\x31.chromiumos.config.api.test.plan.v1.SpecificationJ\x04\x08\x02\x10\x03J\x04\x08\x06\x10\x07J\x04\x08\x07\x10\x08\x42.Z,go.chromium.org/chromiumos/config/go/payloadb\x06proto3')
+  serialized_pb=_b('\n-chromiumos/config/payload/config_bundle.proto\x12\x19\x63hromiumos.config.payload\x1a%chromiumos/config/api/component.proto\x1a\"chromiumos/config/api/design.proto\x1a(chromiumos/config/api/device_brand.proto\x1a#chromiumos/config/api/partner.proto\x1a#chromiumos/config/api/program.proto\x1a-chromiumos/config/api/release/milestone.proto\x1a\x31\x63hromiumos/config/api/software/brand_config.proto\x1a\x31\x63hromiumos/config/api/software/build_target.proto\x1a\x34\x63hromiumos/config/api/software/software_config.proto\x1a-chromiumos/config/api/test/plan/v1/plan.proto\"\x91\x05\n\x0c\x43onfigBundle\x12\x34\n\x0cpartner_list\x18\x0b \x03(\x0b\x32\x1e.chromiumos.config.api.Partner\x12\x34\n\ncomponents\x18\x0c \x03(\x0b\x32 .chromiumos.config.api.Component\x12\x34\n\x0cprogram_list\x18\r \x03(\x0b\x32\x1e.chromiumos.config.api.Program\x12\x32\n\x0b\x64\x65sign_list\x18\x0e \x03(\x0b\x32\x1d.chromiumos.config.api.Design\x12=\n\x11\x64\x65vice_brand_list\x18\x0f \x03(\x0b\x32\".chromiumos.config.api.DeviceBrand\x12\x42\n\rbuild_targets\x18\x08 \x03(\x0b\x32+.chromiumos.config.api.software.BuildTarget\x12H\n\x10software_configs\x18\t \x03(\x0b\x32..chromiumos.config.api.software.SoftwareConfig\x12\x42\n\rbrand_configs\x18\n \x03(\x0b\x32+.chromiumos.config.api.software.BrandConfig\x12<\n\nmilestones\x18\x11 \x03(\x0b\x32(.chromiumos.config.api.release.Milestone\x12J\n\x0ftest_plan_specs\x18\x10 \x03(\x0b\x32\x31.chromiumos.config.api.test.plan.v1.SpecificationJ\x04\x08\x02\x10\x03J\x04\x08\x06\x10\x07J\x04\x08\x07\x10\x08\x42.Z,go.chromium.org/chromiumos/config/go/payloadb\x06proto3')
   ,
-  dependencies=[chromiumos_dot_config_dot_api_dot_component__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_design__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_device__brand__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_partner__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_program__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_brand__config__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_build__target__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_software__config__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_test_dot_plan_dot_v1_dot_plan__pb2.DESCRIPTOR,])
+  dependencies=[chromiumos_dot_config_dot_api_dot_component__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_design__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_device__brand__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_partner__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_program__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_release_dot_milestone__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_brand__config__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_build__target__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_software__config__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_test_dot_plan_dot_v1_dot_plan__pb2.DESCRIPTOR,])
 
 
 
@@ -99,7 +100,14 @@ _CONFIGBUNDLE = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='test_plan_specs', full_name='chromiumos.config.payload.ConfigBundle.test_plan_specs', index=8,
+      name='milestones', full_name='chromiumos.config.payload.ConfigBundle.milestones', index=8,
+      number=17, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='test_plan_specs', full_name='chromiumos.config.payload.ConfigBundle.test_plan_specs', index=9,
       number=16, type=11, cpp_type=10, label=3,
       has_default_value=False, default_value=[],
       message_type=None, enum_type=None, containing_type=None,
@@ -117,8 +125,8 @@ _CONFIGBUNDLE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=471,
-  serialized_end=1066,
+  serialized_start=518,
+  serialized_end=1175,
 )
 
 _CONFIGBUNDLE.fields_by_name['partner_list'].message_type = chromiumos_dot_config_dot_api_dot_partner__pb2._PARTNER
@@ -129,6 +137,7 @@ _CONFIGBUNDLE.fields_by_name['device_brand_list'].message_type = chromiumos_dot_
 _CONFIGBUNDLE.fields_by_name['build_targets'].message_type = chromiumos_dot_config_dot_api_dot_software_dot_build__target__pb2._BUILDTARGET
 _CONFIGBUNDLE.fields_by_name['software_configs'].message_type = chromiumos_dot_config_dot_api_dot_software_dot_software__config__pb2._SOFTWARECONFIG
 _CONFIGBUNDLE.fields_by_name['brand_configs'].message_type = chromiumos_dot_config_dot_api_dot_software_dot_brand__config__pb2._BRANDCONFIG
+_CONFIGBUNDLE.fields_by_name['milestones'].message_type = chromiumos_dot_config_dot_api_dot_release_dot_milestone__pb2._MILESTONE
 _CONFIGBUNDLE.fields_by_name['test_plan_specs'].message_type = chromiumos_dot_config_dot_api_dot_test_dot_plan_dot_v1_dot_plan__pb2._SPECIFICATION
 DESCRIPTOR.message_types_by_name['ConfigBundle'] = _CONFIGBUNDLE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
