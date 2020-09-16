70b7480779c9a6c1186d42a2352ee67395f1557d
C Shapiro <shapiroc@chromium.org>
2020-09-16 12:00:36 -0500

    Initial keyset config for release targets
    See detailed proto comments for more context.
    
    This CL will have a follow up prototype example that demonstrates the
    config usage.
    
    BUG=None
    TEST=./generate
    
    Change-Id: I9e00923beb5df928719501633de1bf7e4cda806a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2414592
    Reviewed-by: George Engelbrecht <engeg@google.com>
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>

--- python/chromiumos/config/prototype/release_target_pb2.py
+++ python/chromiumos/config/prototype/release_target_pb2.py
@@ -21,12 +21,49 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.config.prototype',
   syntax='proto3',
   serialized_options=_b('Z.go.chromium.org/chromiumos/config/go/prototype'),
-  serialized_pb=_b('\n0chromiumos/config/prototype/release_target.proto\x12\x1b\x63hromiumos.config.prototype\x1a\x33\x63hromiumos/config/prototype/release_target_id.proto\x1a\x31\x63hromiumos/config/api/software/system_image.proto\"\xa7\x01\n\rReleaseTarget\x12\x38\n\x02id\x18\x01 \x01(\x0b\x32,.chromiumos.config.prototype.ReleaseTargetId\x12\x0c\n\x04name\x18\x02 \x01(\t\x12N\n\rbuild_targets\x18\x03 \x03(\x0b\x32\x37.chromiumos.config.api.software.SystemImage.BuildTargetB0Z.go.chromium.org/chromiumos/config/go/prototypeb\x06proto3')
+  serialized_pb=_b('\n0chromiumos/config/prototype/release_target.proto\x12\x1b\x63hromiumos.config.prototype\x1a\x33\x63hromiumos/config/prototype/release_target_id.proto\x1a\x31\x63hromiumos/config/api/software/system_image.proto\"\xa0\x02\n\rReleaseTarget\x12\x38\n\x02id\x18\x01 \x01(\x0b\x32,.chromiumos.config.prototype.ReleaseTargetId\x12\x0c\n\x04name\x18\x02 \x01(\t\x12N\n\rbuild_targets\x18\x03 \x03(\x0b\x32\x37.chromiumos.config.api.software.SystemImage.BuildTarget\x12\x43\n\x07signing\x18\x04 \x01(\x0b\x32\x32.chromiumos.config.prototype.ReleaseTarget.Signing\x1a\x32\n\x07Signing\x12\x14\n\x0cpremp_keyset\x18\x01 \x01(\t\x12\x11\n\tmp_keyset\x18\x02 \x01(\tB0Z.go.chromium.org/chromiumos/config/go/prototypeb\x06proto3')
   ,
   dependencies=[chromiumos_dot_config_dot_prototype_dot_release__target__id__pb2.DESCRIPTOR,chromiumos_dot_config_dot_api_dot_software_dot_system__image__pb2.DESCRIPTOR,])
 
 
 
+
+_RELEASETARGET_SIGNING = _descriptor.Descriptor(
+  name='Signing',
+  full_name='chromiumos.config.prototype.ReleaseTarget.Signing',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='premp_keyset', full_name='chromiumos.config.prototype.ReleaseTarget.Signing.premp_keyset', index=0,
+      number=1, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='mp_keyset', full_name='chromiumos.config.prototype.ReleaseTarget.Signing.mp_keyset', index=1,
+      number=2, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=424,
+  serialized_end=474,
+)
 
 _RELEASETARGET = _descriptor.Descriptor(
   name='ReleaseTarget',
@@ -56,10 +93,17 @@ _RELEASETARGET = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='signing', full_name='chromiumos.config.prototype.ReleaseTarget.signing', index=3,
+      number=4, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
-  nested_types=[],
+  nested_types=[_RELEASETARGET_SIGNING, ],
   enum_types=[
   ],
   serialized_options=None,
@@ -69,20 +113,30 @@ _RELEASETARGET = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=186,
-  serialized_end=353,
+  serialized_end=474,
 )
 
+_RELEASETARGET_SIGNING.containing_type = _RELEASETARGET
 _RELEASETARGET.fields_by_name['id'].message_type = chromiumos_dot_config_dot_prototype_dot_release__target__id__pb2._RELEASETARGETID
 _RELEASETARGET.fields_by_name['build_targets'].message_type = chromiumos_dot_config_dot_api_dot_software_dot_system__image__pb2._SYSTEMIMAGE_BUILDTARGET
+_RELEASETARGET.fields_by_name['signing'].message_type = _RELEASETARGET_SIGNING
 DESCRIPTOR.message_types_by_name['ReleaseTarget'] = _RELEASETARGET
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 ReleaseTarget = _reflection.GeneratedProtocolMessageType('ReleaseTarget', (_message.Message,), dict(
+
+  Signing = _reflection.GeneratedProtocolMessageType('Signing', (_message.Message,), dict(
+    DESCRIPTOR = _RELEASETARGET_SIGNING,
+    __module__ = 'chromiumos.config.prototype.release_target_pb2'
+    # @@protoc_insertion_point(class_scope:chromiumos.config.prototype.ReleaseTarget.Signing)
+    ))
+  ,
   DESCRIPTOR = _RELEASETARGET,
   __module__ = 'chromiumos.config.prototype.release_target_pb2'
   # @@protoc_insertion_point(class_scope:chromiumos.config.prototype.ReleaseTarget)
   ))
 _sym_db.RegisterMessage(ReleaseTarget)
+_sym_db.RegisterMessage(ReleaseTarget.Signing)
 
 
 DESCRIPTOR._options = None
