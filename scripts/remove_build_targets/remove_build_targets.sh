635d3b2f27f0561a1dfb396146d99089cc3d9b09
C Shapiro <shapiroc@chromium.org>
2020-12-01 14:57:06 -0600

    Scripting to remove build-targets
    Automation to remove build-targets from all programs.
    
    BUG=None
    TEST=scripts run
    
    Change-Id: I0c27b0c621281968813d2e7eaf5362bc551a7dcb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/2568399
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Sean McAllister <smcallis@google.com>

---
+++ scripts/remove_build_targets/remove_build_targets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Copyright 2020 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Removes build-target config
+
+program=$1
+
+sed -i '/build_target.star/d' ./config.star ./program.star
+sed -i '/build_targets =/d' ./config.star ./program.star
+sed -i '/BUILD_TARGETS/d' ./config.star ./program.star
+./config/bin/gen_config config.star
+git add -u
+
