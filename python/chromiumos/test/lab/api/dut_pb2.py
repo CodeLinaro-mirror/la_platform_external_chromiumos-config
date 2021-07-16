2b37cc78392d1c0a0d2f46689ef32a6797034825
C Shapiro <shapiroc@chromium.org>
2021-07-16 12:47:35 -0500

    Lab-env only must return device config ids
    All of this config will be available as a build artifact for CI builds
    and locally in the src tree for local test execution, so it will be a
    shared service that actually retrieves the config data.
    
    The lab will only be responsible for returning the ids for the lookup.
    In the local lab case, it will scan for the ids via dut_service, but in
    the managed lab, they will be stored/provided to the drone tle.
    
    BUG=b:188712103
    TEST=generate
    
    Change-Id: If9f5b8efa343af4fe029026cb552692d96f7fc20
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3035389
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>
    Reviewed-by: Keith Haddow <haddowk@chromium.org>
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>

--- python/chromiumos/test/lab/api/dut_pb2.py
+++ python/chromiumos/test/lab/api/dut_pb2.py
@@ -12,7 +12,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-from chromiumos.config.api import mfg_config_pb2 as chromiumos_dot_config_dot_api_dot_mfg__config__pb2
+from chromiumos.config.api import device_config_id_pb2 as chromiumos_dot_config_dot_api_dot_device__config__id__pb2
 from chromiumos.test.lab.api import ip_endpoint_pb2 as chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2
 
 
@@ -21,9 +21,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.test.lab.api',
   syntax='proto3',
   serialized_options=_b('Z1go.chromium.org/chromiumos/config/go/test/lab/api'),
-  serialized_pb=_b('\n!chromiumos/test/lab/api/dut.proto\x12\x17\x63hromiumos.test.lab.api\x1a&chromiumos/config/api/mfg_config.proto\x1a)chromiumos/test/lab/api/ip_endpoint.proto\"\xc6\x02\n\x03\x44ut\x12+\n\x02id\x18\x01 \x01(\x0b\x32\x1f.chromiumos.test.lab.api.Dut.Id\x12\x39\n\x08\x63hromeos\x18\x02 \x01(\x0b\x32%.chromiumos.test.lab.api.Dut.ChromeOSH\x00\x12\x37\n\x07\x61ndroid\x18\x03 \x01(\x0b\x32$.chromiumos.test.lab.api.Dut.AndroidH\x00\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\x1ar\n\x08\x43hromeOS\x12\x34\n\nmfg_config\x18\x01 \x01(\x0b\x32 .chromiumos.config.api.MfgConfig\x12\x30\n\x03ssh\x18\x02 \x01(\x0b\x32#.chromiumos.test.lab.api.IpEndpoint\x1a\t\n\x07\x41ndroidB\n\n\x08\x64ut_type\"\xb3\x01\n\x0b\x44utTopology\x12\x33\n\x02id\x18\x03 \x01(\x0b\x32\'.chromiumos.test.lab.api.DutTopology.Id\x12)\n\x03\x64ut\x18\x01 \x01(\x0b\x32\x1c.chromiumos.test.lab.api.Dut\x12/\n\tpeer_duts\x18\x02 \x03(\x0b\x32\x1c.chromiumos.test.lab.api.Dut\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\tB3Z1go.chromium.org/chromiumos/config/go/test/lab/apib\x06proto3')
+  serialized_pb=_b('\n!chromiumos/test/lab/api/dut.proto\x12\x17\x63hromiumos.test.lab.api\x1a,chromiumos/config/api/device_config_id.proto\x1a)chromiumos/test/lab/api/ip_endpoint.proto\"\xd8\x02\n\x03\x44ut\x12+\n\x02id\x18\x01 \x01(\x0b\x32\x1f.chromiumos.test.lab.api.Dut.Id\x12\x39\n\x08\x63hromeos\x18\x02 \x01(\x0b\x32%.chromiumos.test.lab.api.Dut.ChromeOSH\x00\x12\x37\n\x07\x61ndroid\x18\x03 \x01(\x0b\x32$.chromiumos.test.lab.api.Dut.AndroidH\x00\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\x1a\x83\x01\n\x08\x43hromeOS\x12?\n\x10\x64\x65vice_config_id\x18\x03 \x01(\x0b\x32%.chromiumos.config.api.DeviceConfigId\x12\x30\n\x03ssh\x18\x02 \x01(\x0b\x32#.chromiumos.test.lab.api.IpEndpointJ\x04\x08\x01\x10\x02\x1a\t\n\x07\x41ndroidB\n\n\x08\x64ut_type\"\xb3\x01\n\x0b\x44utTopology\x12\x33\n\x02id\x18\x03 \x01(\x0b\x32\'.chromiumos.test.lab.api.DutTopology.Id\x12)\n\x03\x64ut\x18\x01 \x01(\x0b\x32\x1c.chromiumos.test.lab.api.Dut\x12/\n\tpeer_duts\x18\x02 \x03(\x0b\x32\x1c.chromiumos.test.lab.api.Dut\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\tB3Z1go.chromium.org/chromiumos/config/go/test/lab/apib\x06proto3')
   ,
-  dependencies=[chromiumos_dot_config_dot_api_dot_mfg__config__pb2.DESCRIPTOR,chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2.DESCRIPTOR,])
+  dependencies=[chromiumos_dot_config_dot_api_dot_device__config__id__pb2.DESCRIPTOR,chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2.DESCRIPTOR,])
 
 
 
@@ -54,8 +54,8 @@ _DUT_ID = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=314,
-  serialized_end=333,
+  serialized_start=320,
+  serialized_end=339,
 )
 
 _DUT_CHROMEOS = _descriptor.Descriptor(
@@ -66,8 +66,8 @@ _DUT_CHROMEOS = _descriptor.Descriptor(
   containing_type=None,
   fields=[
     _descriptor.FieldDescriptor(
-      name='mfg_config', full_name='chromiumos.test.lab.api.Dut.ChromeOS.mfg_config', index=0,
-      number=1, type=11, cpp_type=10, label=1,
+      name='device_config_id', full_name='chromiumos.test.lab.api.Dut.ChromeOS.device_config_id', index=0,
+      number=3, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -91,8 +91,8 @@ _DUT_CHROMEOS = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=335,
-  serialized_end=449,
+  serialized_start=342,
+  serialized_end=473,
 )
 
 _DUT_ANDROID = _descriptor.Descriptor(
@@ -114,8 +114,8 @@ _DUT_ANDROID = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=451,
-  serialized_end=460,
+  serialized_start=475,
+  serialized_end=484,
 )
 
 _DUT = _descriptor.Descriptor(
@@ -161,8 +161,8 @@ _DUT = _descriptor.Descriptor(
       name='dut_type', full_name='chromiumos.test.lab.api.Dut.dut_type',
       index=0, containing_type=None, fields=[]),
   ],
-  serialized_start=146,
-  serialized_end=472,
+  serialized_start=152,
+  serialized_end=496,
 )
 
 
@@ -192,8 +192,8 @@ _DUTTOPOLOGY_ID = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=314,
-  serialized_end=333,
+  serialized_start=320,
+  serialized_end=339,
 )
 
 _DUTTOPOLOGY = _descriptor.Descriptor(
@@ -236,12 +236,12 @@ _DUTTOPOLOGY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=475,
-  serialized_end=654,
+  serialized_start=499,
+  serialized_end=678,
 )
 
 _DUT_ID.containing_type = _DUT
-_DUT_CHROMEOS.fields_by_name['mfg_config'].message_type = chromiumos_dot_config_dot_api_dot_mfg__config__pb2._MFGCONFIG
+_DUT_CHROMEOS.fields_by_name['device_config_id'].message_type = chromiumos_dot_config_dot_api_dot_device__config__id__pb2._DEVICECONFIGID
 _DUT_CHROMEOS.fields_by_name['ssh'].message_type = chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2._IPENDPOINT
 _DUT_CHROMEOS.containing_type = _DUT
 _DUT_ANDROID.containing_type = _DUT
