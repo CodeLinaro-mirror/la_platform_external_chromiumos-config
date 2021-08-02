3ff29d28a6264a1396a7ce04ff49cce40fb9f6c7
C Shapiro <shapiroc@chromium.org>
2021-08-02 12:54:49 -0500

    Script to add owners to all projects
    BUG=b:195297624
    TEST=refactor cls
    
    Change-Id: Ia698640fe3b0d18f9c4d76ee92954c0f0c6556de
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3065266
    Commit-Queue: C Shapiro <shapiroc@chromium.org>
    Auto-Submit: C Shapiro <shapiroc@chromium.org>
    Reviewed-by: Sean McAllister <smcallis@google.com>

---
+++ scripts/add_all_project_owners/gen_cls.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Copyright 2021 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# See generated commit message for details on script.
+
+project=$1
+name=add-all-project-owners
+
+changes=$(git status | grep "OWNERS")
+if [[ -n "${changes}" ]]; then
+  echo "For ${project}, uploading: ${changes}"
+  git commit -m"
+${project}: Adding all_projects OWNERS
+
+Adding owners to support easy refactors across all programs/projects.
+
+BUG=b:195297624
+TEST=cq
+"
+
+  repo upload --no-verify -y --ht="${name}"
+fi
+
