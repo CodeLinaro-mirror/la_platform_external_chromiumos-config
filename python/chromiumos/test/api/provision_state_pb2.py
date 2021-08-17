1a620fb49e15932f13b5beb28fa778f91293d163
Otabek Kasimov <otabek@google.com>
2021-08-17 09:16:16 -0700

    config: Add flag to prevent rebooting device during provisioning
    BUG=b:195062451
    TEST=none
    
    Change-Id: I5ae7606db20d395b591415779d1421656ef9522e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3100425
    Auto-Submit: Otabek Kasimov <otabek@google.com>
    Commit-Queue: Jaques Clapauch <jaquesc@google.com>
    Commit-Queue: Xianuo Wang <xianuowang@chromium.org>
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Reviewed-by: Xianuo Wang <xianuowang@chromium.org>

--- python/chromiumos/test/api/provision_state_pb2.py
+++ python/chromiumos/test/api/provision_state_pb2.py
@@ -23,7 +23,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.test.api',
   syntax='proto3',
   serialized_options=_b('Z-go.chromium.org/chromiumos/config/go/test/api'),
-  serialized_pb=_b('\n)chromiumos/test/api/provision_state.proto\x12\x13\x63hromiumos.test.api\x1a\x1e\x63hromiumos/build/api/dlc.proto\x1a*chromiumos/build/api/firmware_config.proto\x1a\"chromiumos/build/api/portage.proto\x1a\x1d\x63hromiumos/storage_path.proto\"\x91\x04\n\x0eProvisionState\x12\x32\n\x02id\x18\x01 \x01(\x0b\x32&.chromiumos.test.api.ProvisionState.Id\x12\x36\n\x08\x66irmware\x18\x02 \x01(\x0b\x32$.chromiumos.build.api.FirmwareConfig\x12\x45\n\x0csystem_image\x18\x03 \x01(\x0b\x32/.chromiumos.test.api.ProvisionState.SystemImage\x12=\n\x08packages\x18\x04 \x03(\x0b\x32+.chromiumos.test.api.ProvisionState.Package\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\x1a~\n\x0bSystemImage\x12\x0f\n\x07version\x18\x01 \x01(\t\x12\x32\n\x11system_image_path\x18\x02 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12*\n\x04\x64lcs\x18\x03 \x03(\x0b\x32\x1c.chromiumos.build.api.Dlc.Id\x1ax\n\x07Package\x12>\n\x0fportage_package\x18\x01 \x01(\x0b\x32%.chromiumos.build.api.Portage.Package\x12-\n\x0cpackage_path\x18\x02 \x01(\x0b\x32\x17.chromiumos.StoragePathB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3')
+  serialized_pb=_b('\n)chromiumos/test/api/provision_state.proto\x12\x13\x63hromiumos.test.api\x1a\x1e\x63hromiumos/build/api/dlc.proto\x1a*chromiumos/build/api/firmware_config.proto\x1a\"chromiumos/build/api/portage.proto\x1a\x1d\x63hromiumos/storage_path.proto\"\xa9\x04\n\x0eProvisionState\x12\x32\n\x02id\x18\x01 \x01(\x0b\x32&.chromiumos.test.api.ProvisionState.Id\x12\x36\n\x08\x66irmware\x18\x02 \x01(\x0b\x32$.chromiumos.build.api.FirmwareConfig\x12\x45\n\x0csystem_image\x18\x03 \x01(\x0b\x32/.chromiumos.test.api.ProvisionState.SystemImage\x12=\n\x08packages\x18\x04 \x03(\x0b\x32+.chromiumos.test.api.ProvisionState.Package\x12\x16\n\x0eprevent_reboot\x18\x05 \x01(\x08\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\x1a~\n\x0bSystemImage\x12\x0f\n\x07version\x18\x01 \x01(\t\x12\x32\n\x11system_image_path\x18\x02 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12*\n\x04\x64lcs\x18\x03 \x03(\x0b\x32\x1c.chromiumos.build.api.Dlc.Id\x1ax\n\x07Package\x12>\n\x0fportage_package\x18\x01 \x01(\x0b\x32%.chromiumos.build.api.Portage.Package\x12-\n\x0cpackage_path\x18\x02 \x01(\x0b\x32\x17.chromiumos.StoragePathB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3')
   ,
   dependencies=[chromiumos_dot_build_dot_api_dot_dlc__pb2.DESCRIPTOR,chromiumos_dot_build_dot_api_dot_firmware__config__pb2.DESCRIPTOR,chromiumos_dot_build_dot_api_dot_portage__pb2.DESCRIPTOR,chromiumos_dot_storage__path__pb2.DESCRIPTOR,])
 
@@ -56,8 +56,8 @@ _PROVISIONSTATE_ID = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=470,
-  serialized_end=489,
+  serialized_start=494,
+  serialized_end=513,
 )
 
 _PROVISIONSTATE_SYSTEMIMAGE = _descriptor.Descriptor(
@@ -100,8 +100,8 @@ _PROVISIONSTATE_SYSTEMIMAGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=491,
-  serialized_end=617,
+  serialized_start=515,
+  serialized_end=641,
 )
 
 _PROVISIONSTATE_PACKAGE = _descriptor.Descriptor(
@@ -137,8 +137,8 @@ _PROVISIONSTATE_PACKAGE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=619,
-  serialized_end=739,
+  serialized_start=643,
+  serialized_end=763,
 )
 
 _PROVISIONSTATE = _descriptor.Descriptor(
@@ -176,6 +176,13 @@ _PROVISIONSTATE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='prevent_reboot', full_name='chromiumos.test.api.ProvisionState.prevent_reboot', index=4,
+      number=5, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -189,7 +196,7 @@ _PROVISIONSTATE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=210,
-  serialized_end=739,
+  serialized_end=763,
 )
 
 _PROVISIONSTATE_ID.containing_type = _PROVISIONSTATE
