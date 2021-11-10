b87c73b0c7ef86d31a8f238d512da86af27f3a21
Jack Neus <jackneus@google.com>
2021-11-10 20:20:18 +0000

    setup_project.sh: properly determine root of chromeos checkout
    We shouldn't assume that the script is being invoked from its own
    directory -- if the caller runs `src/config/setup_project.sh`,
    the current logic will set --checkout to the wrong directory.
    
    BUG=b:205272678
    TEST=manual tests
    
    Change-Id: I81fe032b94793d56ac2b93c04ede63be798113cd
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3272158
    Reviewed-by: Sean McAllister <smcallis@google.com>
    Commit-Queue: Jack Neus <jackneus@google.com>

--- setup_project.sh
+++ setup_project.sh
@@ -3,14 +3,29 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# Setup a project.
+find_checkout_root() {
+  path="$(dirname "$0")"
+  while [[ "${path}" != "/" && ! -d "${path}/.repo" ]]
+  do
+    path="$(dirname "${path}")"
+  done
+  echo "${path}"
+}
 
+# Setup a project.
 mkdir /tmp/setup_project 2>/dev/null
 cipd install -force -root /tmp/setup_project \
   chromiumos/infra/setup_project/\$\{platform\} prod
+
 # If --checkout is not supplied, assume that this script is being run from
-# a checkout and infer the root.
+# a checkout. Find the root of the checkout.
 if [[ "$*" != *"-checkout"* ]]; then
-  set -- "$@" "-checkout=$(pwd)/../../"
+  checkout_root="$(find_checkout_root)"
+  if [[ "${checkout_root}" != "/" ]]; then
+    set -- "$@" "-checkout=$(find_checkout_root)/"
+  else
+    echo "You are invoking setup_project.sh from outside of a CrOS checkout."
+    echo "Please specify the location of the checkout with -checkout."
+  fi
 fi
 /tmp/setup_project/setup_project setup-project "$@"
