00d230ec996e17a69abc3c936cdd73761f158a91
Andrew Lamb <andrewlamb@chromium.org>
2022-05-03 17:02:34 +0000

    plan.proto: Add VMTestPlan proto.
    - Similar to HWTestPlan, composed of CoverageRules. Needed for
    compatibility with some repos configured in
    http://cs/chromeos_internal/src/config-internal/board_config/source_tree_test_config_main.star.
    
    BUG=b:182898188
    TEST=CQ
    
    Change-Id: I99f4eff04fbb971eebc9140e8717c1aa8ccdbe0e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3624859
    Auto-Submit: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: Navil Perez <navil@google.com>
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Commit-Queue: Navil Perez <navil@google.com>

--- go/test/api/v1/plan.pb.go
+++ go/test/api/v1/plan.pb.go
@@ -111,15 +111,106 @@ func (m *HWTestPlan_TestPlanId) GetValue() string {
 	return ""
 }
 
+type VMTestPlan struct {
+	Id                   *VMTestPlan_TestPlanId `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	CoverageRules        []*api.CoverageRule    `protobuf:"bytes,2,rep,name=coverage_rules,json=coverageRules,proto3" json:"coverage_rules,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}               `json:"-"`
+	XXX_unrecognized     []byte                 `json:"-"`
+	XXX_sizecache        int32                  `json:"-"`
+}
+
+func (m *VMTestPlan) Reset()         { *m = VMTestPlan{} }
+func (m *VMTestPlan) String() string { return proto.CompactTextString(m) }
+func (*VMTestPlan) ProtoMessage()    {}
+func (*VMTestPlan) Descriptor() ([]byte, []int) {
+	return fileDescriptor_0948466b3cc8b6a2, []int{1}
+}
+
+func (m *VMTestPlan) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_VMTestPlan.Unmarshal(m, b)
+}
+func (m *VMTestPlan) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_VMTestPlan.Marshal(b, m, deterministic)
+}
+func (m *VMTestPlan) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VMTestPlan.Merge(m, src)
+}
+func (m *VMTestPlan) XXX_Size() int {
+	return xxx_messageInfo_VMTestPlan.Size(m)
+}
+func (m *VMTestPlan) XXX_DiscardUnknown() {
+	xxx_messageInfo_VMTestPlan.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VMTestPlan proto.InternalMessageInfo
+
+func (m *VMTestPlan) GetId() *VMTestPlan_TestPlanId {
+	if m != nil {
+		return m.Id
+	}
+	return nil
+}
+
+func (m *VMTestPlan) GetCoverageRules() []*api.CoverageRule {
+	if m != nil {
+		return m.CoverageRules
+	}
+	return nil
+}
+
+// Unique name to identify the test plan, recommend reverse domain name
+// rooted at chromeos:
+//   chromeos.release.wifi....
+type VMTestPlan_TestPlanId struct {
+	Value                string   `protobuf:"bytes,1,opt,name=value,proto3" json:"value,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *VMTestPlan_TestPlanId) Reset()         { *m = VMTestPlan_TestPlanId{} }
+func (m *VMTestPlan_TestPlanId) String() string { return proto.CompactTextString(m) }
+func (*VMTestPlan_TestPlanId) ProtoMessage()    {}
+func (*VMTestPlan_TestPlanId) Descriptor() ([]byte, []int) {
+	return fileDescriptor_0948466b3cc8b6a2, []int{1, 0}
+}
+
+func (m *VMTestPlan_TestPlanId) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_VMTestPlan_TestPlanId.Unmarshal(m, b)
+}
+func (m *VMTestPlan_TestPlanId) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_VMTestPlan_TestPlanId.Marshal(b, m, deterministic)
+}
+func (m *VMTestPlan_TestPlanId) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VMTestPlan_TestPlanId.Merge(m, src)
+}
+func (m *VMTestPlan_TestPlanId) XXX_Size() int {
+	return xxx_messageInfo_VMTestPlan_TestPlanId.Size(m)
+}
+func (m *VMTestPlan_TestPlanId) XXX_DiscardUnknown() {
+	xxx_messageInfo_VMTestPlan_TestPlanId.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VMTestPlan_TestPlanId proto.InternalMessageInfo
+
+func (m *VMTestPlan_TestPlanId) GetValue() string {
+	if m != nil {
+		return m.Value
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*HWTestPlan)(nil), "chromiumos.test.api.v1.HWTestPlan")
 	proto.RegisterType((*HWTestPlan_TestPlanId)(nil), "chromiumos.test.api.v1.HWTestPlan.TestPlanId")
+	proto.RegisterType((*VMTestPlan)(nil), "chromiumos.test.api.v1.VMTestPlan")
+	proto.RegisterType((*VMTestPlan_TestPlanId)(nil), "chromiumos.test.api.v1.VMTestPlan.TestPlanId")
 }
 
 func init() { proto.RegisterFile("chromiumos/test/api/v1/plan.proto", fileDescriptor_0948466b3cc8b6a2) }
 
 var fileDescriptor_0948466b3cc8b6a2 = []byte{
-	// 230 bytes of a gzipped FileDescriptorProto
+	// 242 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x52, 0x4c, 0xce, 0x28, 0xca,
 	0xcf, 0xcd, 0x2c, 0xcd, 0xcd, 0x2f, 0xd6, 0x2f, 0x49, 0x2d, 0x2e, 0xd1, 0x4f, 0x2c, 0xc8, 0xd4,
 	0x2f, 0x33, 0xd4, 0x2f, 0xc8, 0x49, 0xcc, 0xd3, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x12, 0x43,
@@ -130,9 +221,10 @@ var fileDescriptor_0948466b3cc8b6a2 = []byte{
 	0x21, 0xd4, 0xeb, 0xc1, 0x18, 0x9e, 0x29, 0x41, 0x4c, 0x99, 0x29, 0x42, 0x1e, 0x5c, 0x7c, 0x28,
 	0x96, 0x14, 0x4b, 0x30, 0x29, 0x30, 0x6b, 0x70, 0x1b, 0x29, 0x62, 0x35, 0xca, 0x19, 0xaa, 0x34,
 	0xa8, 0x34, 0x27, 0x35, 0x88, 0x37, 0x19, 0x89, 0x57, 0x2c, 0xa5, 0xc4, 0xc5, 0x85, 0x30, 0x5b,
-	0x48, 0x84, 0x8b, 0xb5, 0x2c, 0x31, 0xa7, 0x34, 0x15, 0xec, 0x32, 0xce, 0x20, 0x08, 0xc7, 0xc9,
-	0x2e, 0xca, 0x26, 0x3d, 0x1f, 0x6e, 0xb2, 0x5e, 0x7e, 0x51, 0xba, 0x3e, 0x92, 0xb7, 0x93, 0xf3,
-	0xf3, 0xd2, 0x32, 0xd3, 0xf5, 0xd3, 0xf3, 0x91, 0xc3, 0xce, 0x1a, 0xc4, 0x8e, 0x4f, 0x2c, 0xc8,
-	0x8c, 0x2f, 0x33, 0x4c, 0x62, 0x03, 0x07, 0x81, 0x31, 0x20, 0x00, 0x00, 0xff, 0xff, 0x5c, 0x42,
-	0x65, 0x04, 0x68, 0x01, 0x00, 0x00,
+	0x48, 0x84, 0x8b, 0xb5, 0x2c, 0x31, 0xa7, 0x34, 0x15, 0xec, 0x32, 0xce, 0x20, 0x08, 0x07, 0xec,
+	0xf6, 0x30, 0x5f, 0xd2, 0xdc, 0x8e, 0x50, 0x3f, 0xa0, 0x6e, 0x77, 0xb2, 0x8b, 0xb2, 0x49, 0xcf,
+	0x87, 0x9b, 0xac, 0x97, 0x5f, 0x94, 0xae, 0x8f, 0x14, 0x65, 0xc9, 0xf9, 0x79, 0x69, 0x99, 0xe9,
+	0xfa, 0xe9, 0xf9, 0xc8, 0xf1, 0x6e, 0x0d, 0x62, 0xc7, 0x27, 0x16, 0x64, 0xc6, 0x97, 0x19, 0x26,
+	0xb1, 0x81, 0xa3, 0xcf, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x3a, 0xfd, 0x09, 0x20, 0x24, 0x02,
+	0x00, 0x00,
 }
