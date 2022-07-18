4ae3e41912c1f0d6a857c7be700df6fc1c6343b8
Derek Beckett <dbeckett@chromium.org>
2022-07-18 16:26:30 -0700

    test_suite proto: add testcaseList
    this will allow cros-test-finder to export testcases directly, which
    include deps.
    
    BUG=b:238926102
    TEST=./generate, and the changes to CTF (another CL)
    
    Change-Id: I411e051b9589449e28c793e91faf165d38b7d6f9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3774493
    Reviewed-by: Jard Loucks <jaredloucks@google.com>
    Commit-Queue: Jard Loucks <jaredloucks@google.com>
    Auto-Submit: Derek Beckett <dbeckett@chromium.org>

--- python/chromiumos/test/api/test_case_pb2.py
+++ python/chromiumos/test/api/test_case_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.test.api',
   syntax='proto3',
   serialized_options=_b('Z-go.chromium.org/chromiumos/config/go/test/api'),
-  serialized_pb=_b('\n#chromiumos/test/api/test_case.proto\x12\x13\x63hromiumos.test.api\"\xff\x01\n\x08TestCase\x12,\n\x02id\x18\x01 \x01(\x0b\x32 .chromiumos.test.api.TestCase.Id\x12\x0c\n\x04name\x18\x02 \x01(\t\x12/\n\x04tags\x18\x03 \x03(\x0b\x32!.chromiumos.test.api.TestCase.Tag\x12>\n\x0c\x64\x65pendencies\x18\x04 \x03(\x0b\x32(.chromiumos.test.api.TestCase.Dependency\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\x1a\x14\n\x03Tag\x12\r\n\x05value\x18\x01 \x01(\t\x1a\x1b\n\nDependency\x12\r\n\x05value\x18\x01 \x01(\t\"I\n\x0eTestCaseIdList\x12\x37\n\rtest_case_ids\x18\x01 \x03(\x0b\x32 .chromiumos.test.api.TestCase.IdB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3')
+  serialized_pb=_b('\n#chromiumos/test/api/test_case.proto\x12\x13\x63hromiumos.test.api\"\xff\x01\n\x08TestCase\x12,\n\x02id\x18\x01 \x01(\x0b\x32 .chromiumos.test.api.TestCase.Id\x12\x0c\n\x04name\x18\x02 \x01(\t\x12/\n\x04tags\x18\x03 \x03(\x0b\x32!.chromiumos.test.api.TestCase.Tag\x12>\n\x0c\x64\x65pendencies\x18\x04 \x03(\x0b\x32(.chromiumos.test.api.TestCase.Dependency\x1a\x13\n\x02Id\x12\r\n\x05value\x18\x01 \x01(\t\x1a\x14\n\x03Tag\x12\r\n\x05value\x18\x01 \x01(\t\x1a\x1b\n\nDependency\x12\r\n\x05value\x18\x01 \x01(\t\"I\n\x0eTestCaseIdList\x12\x37\n\rtest_case_ids\x18\x01 \x03(\x0b\x32 .chromiumos.test.api.TestCase.Id\"A\n\x0cTestCaseList\x12\x31\n\ntest_cases\x18\x01 \x03(\x0b\x32\x1d.chromiumos.test.api.TestCaseB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3')
 )
 
 
@@ -197,6 +197,37 @@ _TESTCASEIDLIST = _descriptor.Descriptor(
   serialized_end=391,
 )
 
+
+_TESTCASELIST = _descriptor.Descriptor(
+  name='TestCaseList',
+  full_name='chromiumos.test.api.TestCaseList',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='test_cases', full_name='chromiumos.test.api.TestCaseList.test_cases', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=393,
+  serialized_end=458,
+)
+
 _TESTCASE_ID.containing_type = _TESTCASE
 _TESTCASE_TAG.containing_type = _TESTCASE
 _TESTCASE_DEPENDENCY.containing_type = _TESTCASE
@@ -204,8 +235,10 @@ _TESTCASE.fields_by_name['id'].message_type = _TESTCASE_ID
 _TESTCASE.fields_by_name['tags'].message_type = _TESTCASE_TAG
 _TESTCASE.fields_by_name['dependencies'].message_type = _TESTCASE_DEPENDENCY
 _TESTCASEIDLIST.fields_by_name['test_case_ids'].message_type = _TESTCASE_ID
+_TESTCASELIST.fields_by_name['test_cases'].message_type = _TESTCASE
 DESCRIPTOR.message_types_by_name['TestCase'] = _TESTCASE
 DESCRIPTOR.message_types_by_name['TestCaseIdList'] = _TESTCASEIDLIST
+DESCRIPTOR.message_types_by_name['TestCaseList'] = _TESTCASELIST
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 TestCase = _reflection.GeneratedProtocolMessageType('TestCase', (_message.Message,), dict(
@@ -245,6 +278,13 @@ TestCaseIdList = _reflection.GeneratedProtocolMessageType('TestCaseIdList', (_me
   # @@protoc_insertion_point(class_scope:chromiumos.test.api.TestCaseIdList)
   ))
 _sym_db.RegisterMessage(TestCaseIdList)
+
+TestCaseList = _reflection.GeneratedProtocolMessageType('TestCaseList', (_message.Message,), dict(
+  DESCRIPTOR = _TESTCASELIST,
+  __module__ = 'chromiumos.test.api.test_case_pb2'
+  # @@protoc_insertion_point(class_scope:chromiumos.test.api.TestCaseList)
+  ))
+_sym_db.RegisterMessage(TestCaseList)
 
 
 DESCRIPTOR._options = None
