9a4aa55f631f7b75a518b943286c233e09e16b6e
Azizur Rahman <azrahman@google.com>
2022-08-15 06:41:57 +0000

    cros_publish: add retry count
    changed existing field as it is not being used anywhere yet.
    
    BUG=b:241053779
    TEST=NONE
    
    Change-Id: I08761972417c17916b4e4161edfec2b7ba4a64d5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/3831505
    Auto-Submit: Azizur Rahman <azrahman@google.com>
    Reviewed-by: Jaques Clapauch <jaquesc@google.com>
    Commit-Queue: Jaques Clapauch <jaquesc@google.com>

--- python/chromiumos/test/api/cros_publish_service_pb2.py
+++ python/chromiumos/test/api/cros_publish_service_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='chromiumos.test.api',
   syntax='proto3',
   serialized_options=_b('Z-go.chromium.org/chromiumos/config/go/test/api'),
-  serialized_pb=_b('\n.chromiumos/test/api/cros_publish_service.proto\x12\x13\x63hromiumos.test.api\x1a\'chromiumos/longrunning/operations.proto\x1a\x19google/protobuf/any.proto\x1a\x1d\x63hromiumos/storage_path.proto\"l\n\x0ePublishRequest\x12\x32\n\x11\x61rtifact_dir_path\x18\x01 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12&\n\x08metadata\x18\x02 \x01(\x0b\x32\x14.google.protobuf.Any\"\xd5\x01\n\x0fPublishResponse\x12;\n\x06status\x18\x01 \x01(\x0e\x32+.chromiumos.test.api.PublishResponse.Status\x12\x0f\n\x07message\x18\x02 \x01(\t\x12&\n\x08metadata\x18\x03 \x01(\x0b\x32\x14.google.protobuf.Any\"L\n\x06Status\x12\x12\n\x0eSTATUS_SUCCESS\x10\x00\x12\x1a\n\x16STATUS_INVALID_REQUEST\x10\x01\x12\x12\n\x0eSTATUS_FAILURE\x10\x02\x32\x91\x01\n\x15GenericPublishService\x12x\n\x07Publish\x12#.chromiumos.test.api.PublishRequest\x1a!.chromiumos.longrunning.Operation\"%\xd2\x41\"\n\x0fPublishResponse\x12\x0fPublishMetadataB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3')
+  serialized_pb=_b('\n.chromiumos/test/api/cros_publish_service.proto\x12\x13\x63hromiumos.test.api\x1a\'chromiumos/longrunning/operations.proto\x1a\x19google/protobuf/any.proto\x1a\x1d\x63hromiumos/storage_path.proto\"\x81\x01\n\x0ePublishRequest\x12\x32\n\x11\x61rtifact_dir_path\x18\x01 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12\x13\n\x0bretry_count\x18\x02 \x01(\x05\x12&\n\x08metadata\x18\x03 \x01(\x0b\x32\x14.google.protobuf.Any\"\xd5\x01\n\x0fPublishResponse\x12;\n\x06status\x18\x01 \x01(\x0e\x32+.chromiumos.test.api.PublishResponse.Status\x12\x0f\n\x07message\x18\x02 \x01(\t\x12&\n\x08metadata\x18\x03 \x01(\x0b\x32\x14.google.protobuf.Any\"L\n\x06Status\x12\x12\n\x0eSTATUS_SUCCESS\x10\x00\x12\x1a\n\x16STATUS_INVALID_REQUEST\x10\x01\x12\x12\n\x0eSTATUS_FAILURE\x10\x02\x32\x91\x01\n\x15GenericPublishService\x12x\n\x07Publish\x12#.chromiumos.test.api.PublishRequest\x1a!.chromiumos.longrunning.Operation\"%\xd2\x41\"\n\x0fPublishResponse\x12\x0fPublishMetadataB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3')
   ,
   dependencies=[chromiumos_dot_longrunning_dot_operations__pb2.DESCRIPTOR,google_dot_protobuf_dot_any__pb2.DESCRIPTOR,chromiumos_dot_storage__path__pb2.DESCRIPTOR,])
 
@@ -49,8 +49,8 @@ _PUBLISHRESPONSE_STATUS = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=418,
-  serialized_end=494,
+  serialized_start=440,
+  serialized_end=516,
 )
 _sym_db.RegisterEnumDescriptor(_PUBLISHRESPONSE_STATUS)
 
@@ -70,8 +70,15 @@ _PUBLISHREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='metadata', full_name='chromiumos.test.api.PublishRequest.metadata', index=1,
-      number=2, type=11, cpp_type=10, label=1,
+      name='retry_count', full_name='chromiumos.test.api.PublishRequest.retry_count', index=1,
+      number=2, type=5, cpp_type=1, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='metadata', full_name='chromiumos.test.api.PublishRequest.metadata', index=2,
+      number=3, type=11, cpp_type=10, label=1,
       has_default_value=False, default_value=None,
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
@@ -88,8 +95,8 @@ _PUBLISHREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=170,
-  serialized_end=278,
+  serialized_start=171,
+  serialized_end=300,
 )
 
 
@@ -134,8 +141,8 @@ _PUBLISHRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=281,
-  serialized_end=494,
+  serialized_start=303,
+  serialized_end=516,
 )
 
 _PUBLISHREQUEST.fields_by_name['artifact_dir_path'].message_type = chromiumos_dot_storage__path__pb2._STORAGEPATH
@@ -170,8 +177,8 @@ _GENERICPUBLISHSERVICE = _descriptor.ServiceDescriptor(
   file=DESCRIPTOR,
   index=0,
   serialized_options=None,
-  serialized_start=497,
-  serialized_end=642,
+  serialized_start=519,
+  serialized_end=664,
   methods=[
   _descriptor.MethodDescriptor(
     name='Publish',
