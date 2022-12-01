226e9aab260a1c7139973b37ac0c630a594a1080
Tobias Ephron <ephront@google.com>
2022-12-01 21:22:22 +0000

    Tko proto update to include new "uploader" field.
    A new "uploader" field will be recorded and populated for test results, so the field needs to be added to the existing tko proto.
    BUG=b:251492300
    
    TEST=N/A
    
    Change-Id: I1e4c014a39188dfffc49dcfd5711c8edd85d59d3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/4072119
    Reviewed-by: Parker Holloway <jparkerh@google.com>
    Commit-Queue: Tobias Ephron <ephront@google.com>
    Reviewed-by: Jason Kusuma <jkusuma@google.com>

--- python/chromiumos/test/artifact/tko_pb2.py
+++ python/chromiumos/test/artifact/tko_pb2.py
@@ -19,7 +19,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto2',
   serialized_options=b'Z2go.chromium.org/chromiumos/config/go/test/artifact',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\"chromiumos/test/artifact/tko.proto\x12\x18\x63hromiumos.test.artifact\"\xa9\x08\n\x03Job\x12\x0b\n\x03\x64ir\x18\x01 \x02(\t\x12\x31\n\x05tests\x18\x02 \x03(\x0b\x32\".chromiumos.test.artifact.Job.Test\x12\x0c\n\x04user\x18\x03 \x02(\t\x12\r\n\x05label\x18\x04 \x02(\t\x12\x0b\n\x03tag\x18\x05 \x02(\t\x12\x13\n\x0bqueued_time\x18\x06 \x02(\x03\x12\x14\n\x0cstarted_time\x18\x07 \x02(\x03\x12\x15\n\rfinished_time\x18\x08 \x02(\x03\x12\x0f\n\x07machine\x18\t \x02(\t\x12\x15\n\rmachine_owner\x18\n \x02(\t\x12\x15\n\rmachine_group\x18\x0b \x02(\t\x12\x12\n\naborted_by\x18\x0c \x02(\t\x12\x12\n\naborted_on\x18\r \x02(\x03\x12\x12\n\nafe_job_id\x18\x0e \x02(\t\x12\x39\n\x0bkeyval_dict\x18\x0f \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x12\x19\n\x11\x61\x66\x65_parent_job_id\x18\x10 \x01(\t\x12\x0f\n\x07job_idx\x18\x11 \x01(\x03\x12\x15\n\rbuild_version\x18\x12 \x01(\t\x12\r\n\x05suite\x18\x13 \x01(\t\x12\r\n\x05\x62oard\x18\x14 \x01(\t\x12\x12\n\ndlm_sku_id\x18\x15 \x01(\t\x12\x0c\n\x04hwid\x18\x16 \x01(\t\x1a%\n\x06KeyVal\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\r\n\x05value\x18\x02 \x02(\t\x1a+\n\x06Kernel\x12\x0c\n\x04\x62\x61se\x18\x01 \x02(\t\x12\x13\n\x0bkernel_hash\x18\x02 \x02(\t\x1a\x90\x01\n\tIteration\x12\r\n\x05index\x18\x01 \x02(\x03\x12\x39\n\x0b\x61ttr_keyval\x18\x02 \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x12\x39\n\x0bperf_keyval\x18\x03 \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x1a\xd5\x02\n\x04Test\x12\x0e\n\x06subdir\x18\x01 \x02(\t\x12\x10\n\x08testname\x18\x02 \x02(\t\x12\x0e\n\x06status\x18\x03 \x02(\t\x12\x0e\n\x06reason\x18\x04 \x02(\t\x12\x34\n\x06kernel\x18\x05 \x02(\x0b\x32$.chromiumos.test.artifact.Job.Kernel\x12\x0f\n\x07machine\x18\x06 \x02(\t\x12\x14\n\x0cstarted_time\x18\x07 \x02(\x03\x12\x15\n\rfinished_time\x18\x08 \x02(\x03\x12;\n\niterations\x18\t \x03(\x0b\x32\'.chromiumos.test.artifact.Job.Iteration\x12\x38\n\nattributes\x18\n \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x12\x0e\n\x06labels\x18\x0b \x03(\t\x12\x10\n\x08test_idx\x18\x0c \x01(\x03\x42\x34Z2go.chromium.org/chromiumos/config/go/test/artifact'
+  serialized_pb=b'\n\"chromiumos/test/artifact/tko.proto\x12\x18\x63hromiumos.test.artifact\"\xbb\x08\n\x03Job\x12\x0b\n\x03\x64ir\x18\x01 \x02(\t\x12\x31\n\x05tests\x18\x02 \x03(\x0b\x32\".chromiumos.test.artifact.Job.Test\x12\x0c\n\x04user\x18\x03 \x02(\t\x12\r\n\x05label\x18\x04 \x02(\t\x12\x0b\n\x03tag\x18\x05 \x02(\t\x12\x13\n\x0bqueued_time\x18\x06 \x02(\x03\x12\x14\n\x0cstarted_time\x18\x07 \x02(\x03\x12\x15\n\rfinished_time\x18\x08 \x02(\x03\x12\x0f\n\x07machine\x18\t \x02(\t\x12\x15\n\rmachine_owner\x18\n \x02(\t\x12\x15\n\rmachine_group\x18\x0b \x02(\t\x12\x12\n\naborted_by\x18\x0c \x02(\t\x12\x12\n\naborted_on\x18\r \x02(\x03\x12\x12\n\nafe_job_id\x18\x0e \x02(\t\x12\x39\n\x0bkeyval_dict\x18\x0f \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x12\x19\n\x11\x61\x66\x65_parent_job_id\x18\x10 \x01(\t\x12\x0f\n\x07job_idx\x18\x11 \x01(\x03\x12\x15\n\rbuild_version\x18\x12 \x01(\t\x12\r\n\x05suite\x18\x13 \x01(\t\x12\r\n\x05\x62oard\x18\x14 \x01(\t\x12\x12\n\ndlm_sku_id\x18\x15 \x01(\t\x12\x0c\n\x04hwid\x18\x16 \x01(\t\x12\x10\n\x08uploader\x18\x17 \x01(\t\x1a%\n\x06KeyVal\x12\x0c\n\x04name\x18\x01 \x02(\t\x12\r\n\x05value\x18\x02 \x02(\t\x1a+\n\x06Kernel\x12\x0c\n\x04\x62\x61se\x18\x01 \x02(\t\x12\x13\n\x0bkernel_hash\x18\x02 \x02(\t\x1a\x90\x01\n\tIteration\x12\r\n\x05index\x18\x01 \x02(\x03\x12\x39\n\x0b\x61ttr_keyval\x18\x02 \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x12\x39\n\x0bperf_keyval\x18\x03 \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x1a\xd5\x02\n\x04Test\x12\x0e\n\x06subdir\x18\x01 \x02(\t\x12\x10\n\x08testname\x18\x02 \x02(\t\x12\x0e\n\x06status\x18\x03 \x02(\t\x12\x0e\n\x06reason\x18\x04 \x02(\t\x12\x34\n\x06kernel\x18\x05 \x02(\x0b\x32$.chromiumos.test.artifact.Job.Kernel\x12\x0f\n\x07machine\x18\x06 \x02(\t\x12\x14\n\x0cstarted_time\x18\x07 \x02(\x03\x12\x15\n\rfinished_time\x18\x08 \x02(\x03\x12;\n\niterations\x18\t \x03(\x0b\x32\'.chromiumos.test.artifact.Job.Iteration\x12\x38\n\nattributes\x18\n \x03(\x0b\x32$.chromiumos.test.artifact.Job.KeyVal\x12\x0e\n\x06labels\x18\x0b \x03(\t\x12\x10\n\x08test_idx\x18\x0c \x01(\x03\x42\x34Z2go.chromium.org/chromiumos/config/go/test/artifact'
 )
 
 
@@ -59,8 +59,8 @@ _JOB_KEYVAL = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=557,
-  serialized_end=594,
+  serialized_start=575,
+  serialized_end=612,
 )
 
 _JOB_KERNEL = _descriptor.Descriptor(
@@ -97,8 +97,8 @@ _JOB_KERNEL = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=596,
-  serialized_end=639,
+  serialized_start=614,
+  serialized_end=657,
 )
 
 _JOB_ITERATION = _descriptor.Descriptor(
@@ -142,8 +142,8 @@ _JOB_ITERATION = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=642,
-  serialized_end=786,
+  serialized_start=660,
+  serialized_end=804,
 )
 
 _JOB_TEST = _descriptor.Descriptor(
@@ -250,8 +250,8 @@ _JOB_TEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=789,
-  serialized_end=1130,
+  serialized_start=807,
+  serialized_end=1148,
 )
 
 _JOB = _descriptor.Descriptor(
@@ -416,6 +416,13 @@ _JOB = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='uploader', full_name='chromiumos.test.artifact.Job.uploader', index=22,
+      number=23, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=b"".decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -429,7 +436,7 @@ _JOB = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=65,
-  serialized_end=1130,
+  serialized_end=1148,
 )
 
 _JOB_KEYVAL.containing_type = _JOB
