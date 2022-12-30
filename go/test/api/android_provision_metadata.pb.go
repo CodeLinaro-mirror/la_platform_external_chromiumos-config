99389daf7aad4cda47c67475b3ccfd5682710b7a
Sergey Fetisov <sfetisov@google.com>
2022-12-29 17:10:20 -0800

    Refactoring Android provision protos
    This CL adds AndroidProvisionResponseMetadata to return a list of provisioned Android packages and decouples the existing Android CLI proto to simplify future deprecation.
    
    Change-Id: Id574890ace634620d77c29110b6108c32629b60e
    Bug=b:261230492
    TEST=./generate.sh --allow-breaking
    TEST=bin/gen_config test/project/fake/fake/config.star
    TEST=./run_py_unittests.sh
    TEST=./run_go_unittests.sh
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/4127178
    Commit-Queue: Sergey Fetisov <sfetisov@google.com>
    Reviewed-by: Garry Wang <xianuowang@chromium.org>
    Reviewed-by: Otabek Kasimov <otabek@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>

--- go/test/api/android_provision_metadata.pb.go
+++ go/test/api/android_provision_metadata.pb.go
@@ -523,7 +523,7 @@ func (*CIPDPackage_Tag) isCIPDPackage_VersionOneof() {}
 func (*CIPDPackage_InstanceId) isCIPDPackage_VersionOneof() {}
 
 // Next Tag: 2
-type AndroidProvisionMetadata struct {
+type AndroidProvisionRequestMetadata struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
@@ -532,8 +532,8 @@ type AndroidProvisionMetadata struct {
 	CipdPackages []*CIPDPackage `protobuf:"bytes,1,rep,name=cipd_packages,json=cipdPackages,proto3" json:"cipd_packages,omitempty"`
 }
 
-func (x *AndroidProvisionMetadata) Reset() {
-	*x = AndroidProvisionMetadata{}
+func (x *AndroidProvisionRequestMetadata) Reset() {
+	*x = AndroidProvisionRequestMetadata{}
 	if protoimpl.UnsafeEnabled {
 		mi := &file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -541,13 +541,13 @@ func (x *AndroidProvisionMetadata) Reset() {
 	}
 }
 
-func (x *AndroidProvisionMetadata) String() string {
+func (x *AndroidProvisionRequestMetadata) String() string {
 	return protoimpl.X.MessageStringOf(x)
 }
 
-func (*AndroidProvisionMetadata) ProtoMessage() {}
+func (*AndroidProvisionRequestMetadata) ProtoMessage() {}
 
-func (x *AndroidProvisionMetadata) ProtoReflect() protoreflect.Message {
+func (x *AndroidProvisionRequestMetadata) ProtoReflect() protoreflect.Message {
 	mi := &file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
@@ -559,14 +559,121 @@ func (x *AndroidProvisionMetadata) ProtoReflect() protoreflect.Message {
 	return mi.MessageOf(x)
 }
 
-// Deprecated: Use AndroidProvisionMetadata.ProtoReflect.Descriptor instead.
-func (*AndroidProvisionMetadata) Descriptor() ([]byte, []int) {
+// Deprecated: Use AndroidProvisionRequestMetadata.ProtoReflect.Descriptor instead.
+func (*AndroidProvisionRequestMetadata) Descriptor() ([]byte, []int) {
 	return file_chromiumos_test_api_android_provision_metadata_proto_rawDescGZIP(), []int{2}
 }
 
-func (x *AndroidProvisionMetadata) GetCipdPackages() []*CIPDPackage {
+func (x *AndroidProvisionRequestMetadata) GetCipdPackages() []*CIPDPackage {
 	if x != nil {
 		return x.CipdPackages
+	}
+	return nil
+}
+
+// Next Tag: 3
+type InstalledAndroidPackage struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// Required. Name of the installed package.
+	Name string `protobuf:"bytes,1,opt,name=name,proto3" json:"name,omitempty"`
+	// Required. Version of the installed package.
+	VersionCode string `protobuf:"bytes,2,opt,name=version_code,json=versionCode,proto3" json:"version_code,omitempty"`
+}
+
+func (x *InstalledAndroidPackage) Reset() {
+	*x = InstalledAndroidPackage{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[3]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *InstalledAndroidPackage) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*InstalledAndroidPackage) ProtoMessage() {}
+
+func (x *InstalledAndroidPackage) ProtoReflect() protoreflect.Message {
+	mi := &file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[3]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use InstalledAndroidPackage.ProtoReflect.Descriptor instead.
+func (*InstalledAndroidPackage) Descriptor() ([]byte, []int) {
+	return file_chromiumos_test_api_android_provision_metadata_proto_rawDescGZIP(), []int{3}
+}
+
+func (x *InstalledAndroidPackage) GetName() string {
+	if x != nil {
+		return x.Name
+	}
+	return ""
+}
+
+func (x *InstalledAndroidPackage) GetVersionCode() string {
+	if x != nil {
+		return x.VersionCode
+	}
+	return ""
+}
+
+// Next Tag: 2
+type AndroidProvisionResponseMetadata struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	// List of the provisioned Android packages.
+	InstalledAndroidPackages []*InstalledAndroidPackage `protobuf:"bytes,1,rep,name=installed_android_packages,json=installedAndroidPackages,proto3" json:"installed_android_packages,omitempty"`
+}
+
+func (x *AndroidProvisionResponseMetadata) Reset() {
+	*x = AndroidProvisionResponseMetadata{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[4]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *AndroidProvisionResponseMetadata) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*AndroidProvisionResponseMetadata) ProtoMessage() {}
+
+func (x *AndroidProvisionResponseMetadata) ProtoReflect() protoreflect.Message {
+	mi := &file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[4]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use AndroidProvisionResponseMetadata.ProtoReflect.Descriptor instead.
+func (*AndroidProvisionResponseMetadata) Descriptor() ([]byte, []int) {
+	return file_chromiumos_test_api_android_provision_metadata_proto_rawDescGZIP(), []int{4}
+}
+
+func (x *AndroidProvisionResponseMetadata) GetInstalledAndroidPackages() []*InstalledAndroidPackage {
+	if x != nil {
+		return x.InstalledAndroidPackages
 	}
 	return nil
 }
@@ -647,21 +754,35 @@ var file_chromiumos_test_api_android_provision_metadata_proto_rawDesc = []byte{
 	0x6d, 0x69, 0x75, 0x6d, 0x6f, 0x73, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x61, 0x70, 0x69, 0x2e,
 	0x41, 0x70, 0x6b, 0x44, 0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x52, 0x0a, 0x61, 0x70, 0x6b, 0x44,
 	0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x42, 0x0f, 0x0a, 0x0d, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f,
-	0x6e, 0x5f, 0x6f, 0x6e, 0x65, 0x6f, 0x66, 0x22, 0x61, 0x0a, 0x18, 0x41, 0x6e, 0x64, 0x72, 0x6f,
-	0x69, 0x64, 0x50, 0x72, 0x6f, 0x76, 0x69, 0x73, 0x69, 0x6f, 0x6e, 0x4d, 0x65, 0x74, 0x61, 0x64,
-	0x61, 0x74, 0x61, 0x12, 0x45, 0x0a, 0x0d, 0x63, 0x69, 0x70, 0x64, 0x5f, 0x70, 0x61, 0x63, 0x6b,
-	0x61, 0x67, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x20, 0x2e, 0x63, 0x68, 0x72,
-	0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x6f, 0x73, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x61, 0x70, 0x69,
-	0x2e, 0x43, 0x49, 0x50, 0x44, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x52, 0x0c, 0x63, 0x69,
-	0x70, 0x64, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x73, 0x2a, 0x3f, 0x0a, 0x0e, 0x41, 0x6e,
-	0x64, 0x72, 0x6f, 0x69, 0x64, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x12, 0x1f, 0x0a, 0x1b,
-	0x41, 0x4e, 0x44, 0x52, 0x4f, 0x49, 0x44, 0x5f, 0x50, 0x41, 0x43, 0x4b, 0x41, 0x47, 0x45, 0x5f,
-	0x55, 0x4e, 0x53, 0x50, 0x45, 0x43, 0x49, 0x46, 0x49, 0x45, 0x44, 0x10, 0x00, 0x12, 0x0c, 0x0a,
-	0x08, 0x47, 0x4d, 0x53, 0x5f, 0x43, 0x4f, 0x52, 0x45, 0x10, 0x01, 0x42, 0x2f, 0x5a, 0x2d, 0x67,
-	0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x2e, 0x6f, 0x72, 0x67, 0x2f, 0x63,
-	0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x6f, 0x73, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67,
-	0x2f, 0x67, 0x6f, 0x2f, 0x74, 0x65, 0x73, 0x74, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x6e, 0x5f, 0x6f, 0x6e, 0x65, 0x6f, 0x66, 0x22, 0x68, 0x0a, 0x1f, 0x41, 0x6e, 0x64, 0x72, 0x6f,
+	0x69, 0x64, 0x50, 0x72, 0x6f, 0x76, 0x69, 0x73, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x71, 0x75, 0x65,
+	0x73, 0x74, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x12, 0x45, 0x0a, 0x0d, 0x63, 0x69,
+	0x70, 0x64, 0x5f, 0x70, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28,
+	0x0b, 0x32, 0x20, 0x2e, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x6f, 0x73, 0x2e, 0x74,
+	0x65, 0x73, 0x74, 0x2e, 0x61, 0x70, 0x69, 0x2e, 0x43, 0x49, 0x50, 0x44, 0x50, 0x61, 0x63, 0x6b,
+	0x61, 0x67, 0x65, 0x52, 0x0c, 0x63, 0x69, 0x70, 0x64, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65,
+	0x73, 0x22, 0x50, 0x0a, 0x17, 0x49, 0x6e, 0x73, 0x74, 0x61, 0x6c, 0x6c, 0x65, 0x64, 0x41, 0x6e,
+	0x64, 0x72, 0x6f, 0x69, 0x64, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x12, 0x12, 0x0a, 0x04,
+	0x6e, 0x61, 0x6d, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04, 0x6e, 0x61, 0x6d, 0x65,
+	0x12, 0x21, 0x0a, 0x0c, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x5f, 0x63, 0x6f, 0x64, 0x65,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x76, 0x65, 0x72, 0x73, 0x69, 0x6f, 0x6e, 0x43,
+	0x6f, 0x64, 0x65, 0x22, 0x8e, 0x01, 0x0a, 0x20, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x50,
+	0x72, 0x6f, 0x76, 0x69, 0x73, 0x69, 0x6f, 0x6e, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65,
+	0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x12, 0x6a, 0x0a, 0x1a, 0x69, 0x6e, 0x73, 0x74,
+	0x61, 0x6c, 0x6c, 0x65, 0x64, 0x5f, 0x61, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x5f, 0x70, 0x61,
+	0x63, 0x6b, 0x61, 0x67, 0x65, 0x73, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32, 0x2c, 0x2e, 0x63,
+	0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75, 0x6d, 0x6f, 0x73, 0x2e, 0x74, 0x65, 0x73, 0x74, 0x2e, 0x61,
+	0x70, 0x69, 0x2e, 0x49, 0x6e, 0x73, 0x74, 0x61, 0x6c, 0x6c, 0x65, 0x64, 0x41, 0x6e, 0x64, 0x72,
+	0x6f, 0x69, 0x64, 0x50, 0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x52, 0x18, 0x69, 0x6e, 0x73, 0x74,
+	0x61, 0x6c, 0x6c, 0x65, 0x64, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x50, 0x61, 0x63, 0x6b,
+	0x61, 0x67, 0x65, 0x73, 0x2a, 0x3f, 0x0a, 0x0e, 0x41, 0x6e, 0x64, 0x72, 0x6f, 0x69, 0x64, 0x50,
+	0x61, 0x63, 0x6b, 0x61, 0x67, 0x65, 0x12, 0x1f, 0x0a, 0x1b, 0x41, 0x4e, 0x44, 0x52, 0x4f, 0x49,
+	0x44, 0x5f, 0x50, 0x41, 0x43, 0x4b, 0x41, 0x47, 0x45, 0x5f, 0x55, 0x4e, 0x53, 0x50, 0x45, 0x43,
+	0x49, 0x46, 0x49, 0x45, 0x44, 0x10, 0x00, 0x12, 0x0c, 0x0a, 0x08, 0x47, 0x4d, 0x53, 0x5f, 0x43,
+	0x4f, 0x52, 0x45, 0x10, 0x01, 0x42, 0x2f, 0x5a, 0x2d, 0x67, 0x6f, 0x2e, 0x63, 0x68, 0x72, 0x6f,
+	0x6d, 0x69, 0x75, 0x6d, 0x2e, 0x6f, 0x72, 0x67, 0x2f, 0x63, 0x68, 0x72, 0x6f, 0x6d, 0x69, 0x75,
+	0x6d, 0x6f, 0x73, 0x2f, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2f, 0x67, 0x6f, 0x2f, 0x74, 0x65,
+	0x73, 0x74, 0x2f, 0x61, 0x70, 0x69, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -677,16 +798,18 @@ func file_chromiumos_test_api_android_provision_metadata_proto_rawDescGZIP() []b
 }
 
 var file_chromiumos_test_api_android_provision_metadata_proto_enumTypes = make([]protoimpl.EnumInfo, 5)
-var file_chromiumos_test_api_android_provision_metadata_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_chromiumos_test_api_android_provision_metadata_proto_msgTypes = make([]protoimpl.MessageInfo, 5)
 var file_chromiumos_test_api_android_provision_metadata_proto_goTypes = []interface{}{
-	(AndroidPackage)(0),              // 0: chromiumos.test.api.AndroidPackage
-	(ApkDetails_Architecture)(0),     // 1: chromiumos.test.api.ApkDetails.Architecture
-	(ApkDetails_BuildType)(0),        // 2: chromiumos.test.api.ApkDetails.BuildType
-	(ApkDetails_BuildPurpose)(0),     // 3: chromiumos.test.api.ApkDetails.BuildPurpose
-	(ApkDetails_Density)(0),          // 4: chromiumos.test.api.ApkDetails.Density
-	(*ApkDetails)(nil),               // 5: chromiumos.test.api.ApkDetails
-	(*CIPDPackage)(nil),              // 6: chromiumos.test.api.CIPDPackage
-	(*AndroidProvisionMetadata)(nil), // 7: chromiumos.test.api.AndroidProvisionMetadata
+	(AndroidPackage)(0),                      // 0: chromiumos.test.api.AndroidPackage
+	(ApkDetails_Architecture)(0),             // 1: chromiumos.test.api.ApkDetails.Architecture
+	(ApkDetails_BuildType)(0),                // 2: chromiumos.test.api.ApkDetails.BuildType
+	(ApkDetails_BuildPurpose)(0),             // 3: chromiumos.test.api.ApkDetails.BuildPurpose
+	(ApkDetails_Density)(0),                  // 4: chromiumos.test.api.ApkDetails.Density
+	(*ApkDetails)(nil),                       // 5: chromiumos.test.api.ApkDetails
+	(*CIPDPackage)(nil),                      // 6: chromiumos.test.api.CIPDPackage
+	(*AndroidProvisionRequestMetadata)(nil),  // 7: chromiumos.test.api.AndroidProvisionRequestMetadata
+	(*InstalledAndroidPackage)(nil),          // 8: chromiumos.test.api.InstalledAndroidPackage
+	(*AndroidProvisionResponseMetadata)(nil), // 9: chromiumos.test.api.AndroidProvisionResponseMetadata
 }
 var file_chromiumos_test_api_android_provision_metadata_proto_depIdxs = []int32{
 	1, // 0: chromiumos.test.api.ApkDetails.architecture:type_name -> chromiumos.test.api.ApkDetails.Architecture
@@ -695,12 +818,13 @@ var file_chromiumos_test_api_android_provision_metadata_proto_depIdxs = []int32{
 	4, // 3: chromiumos.test.api.ApkDetails.density:type_name -> chromiumos.test.api.ApkDetails.Density
 	0, // 4: chromiumos.test.api.CIPDPackage.android_package:type_name -> chromiumos.test.api.AndroidPackage
 	5, // 5: chromiumos.test.api.CIPDPackage.apk_details:type_name -> chromiumos.test.api.ApkDetails
-	6, // 6: chromiumos.test.api.AndroidProvisionMetadata.cipd_packages:type_name -> chromiumos.test.api.CIPDPackage
-	7, // [7:7] is the sub-list for method output_type
-	7, // [7:7] is the sub-list for method input_type
-	7, // [7:7] is the sub-list for extension type_name
-	7, // [7:7] is the sub-list for extension extendee
-	0, // [0:7] is the sub-list for field type_name
+	6, // 6: chromiumos.test.api.AndroidProvisionRequestMetadata.cipd_packages:type_name -> chromiumos.test.api.CIPDPackage
+	8, // 7: chromiumos.test.api.AndroidProvisionResponseMetadata.installed_android_packages:type_name -> chromiumos.test.api.InstalledAndroidPackage
+	8, // [8:8] is the sub-list for method output_type
+	8, // [8:8] is the sub-list for method input_type
+	8, // [8:8] is the sub-list for extension type_name
+	8, // [8:8] is the sub-list for extension extendee
+	0, // [0:8] is the sub-list for field type_name
 }
 
 func init() { file_chromiumos_test_api_android_provision_metadata_proto_init() }
@@ -734,7 +858,31 @@ func file_chromiumos_test_api_android_provision_metadata_proto_init() {
 			}
 		}
 		file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
-			switch v := v.(*AndroidProvisionMetadata); i {
+			switch v := v.(*AndroidProvisionRequestMetadata); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[3].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*InstalledAndroidPackage); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_chromiumos_test_api_android_provision_metadata_proto_msgTypes[4].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*AndroidProvisionResponseMetadata); i {
 			case 0:
 				return &v.state
 			case 1:
@@ -757,7 +905,7 @@ func file_chromiumos_test_api_android_provision_metadata_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_chromiumos_test_api_android_provision_metadata_proto_rawDesc,
 			NumEnums:      5,
-			NumMessages:   3,
+			NumMessages:   5,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
