d6af4f3aeb04e1e378c6436b750f15633a021c30
Navil Perez <navil@google.com>
2023-04-14 19:33:14 +0000

    config_to_datastore: Fix path to device_stabilit.cfg, take 2.
    I had the wrong path before. This one should be it!
    
    BUG=b:276814333
    TEST=CQ
    
    Change-Id: I7ff84616ca45db1a0c77b4a69a6ea9e2095bd617
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/4424866
    Commit-Queue: Andrew Lamb <andrewlamb@chromium.org>
    Reviewed-by: Andrew Lamb <andrewlamb@chromium.org>
    Auto-Submit: Navil Perez <navil@google.com>

--- payload_utils/config_to_datastore.py
+++ payload_utils/config_to_datastore.py
@@ -236,5 +236,5 @@ if __name__ == '__main__':
       os.path.realpath(
           os.path.join(
               script_dir,
-              "../../config-internal/testingconfig/generated/device_stability.cfg"
+              "../../config-internal/board_config/generated/device_stability.cfg"
           )), ufs_ds_client)
