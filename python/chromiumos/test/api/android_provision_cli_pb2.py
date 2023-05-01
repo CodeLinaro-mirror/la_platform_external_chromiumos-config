fb364b5f00ab4285b527102d811ca58f3c51d7f7
Sergey Fetisov <sfetisov@google.com>
2023-05-01 10:42:31 -0700

    Adding provisioned Android OS version field to the Android Provision CLI response proto
    Change-Id: I51e79473a207977f3d4bed18154998eee413561f
    Bug=b:280310863
    
    TEST=./generate.sh --allow-breaking
    TEST=bin/gen_config test/project/fake/fake/config.star
    TEST=./run_py_unittests.sh
    TEST=./run_go_unittests.sh
    
    Change-Id: I80bfc853303a6e9813d9355bfbc162a43ce98b23
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/4496626
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Sergey Fetisov <sfetisov@google.com>
    Reviewed-by: Kyle Shimabukuro <kyleshima@chromium.org>

--- python/chromiumos/test/api/android_provision_cli_pb2.py
+++ python/chromiumos/test/api/android_provision_cli_pb2.py
@@ -22,7 +22,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z-go.chromium.org/chromiumos/config/go/test/api',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n/chromiumos/test/api/android_provision_cli.proto\x12\x13\x63hromiumos.test.api\x1a\x34\x63hromiumos/test/api/android_provision_metadata.proto\x1a+chromiumos/test/api/provision_service.proto\x1a!chromiumos/test/lab/api/dut.proto\"\x8d\x02\n\x1b\x41ndroidProvisionCLIResponse\x12+\n\x02id\x18\x01 \x01(\x0b\x32\x1f.chromiumos.test.lab.api.Dut.Id\x12\x36\n\x07success\x18\x02 \x01(\x0b\x32#.chromiumos.test.api.InstallSuccessH\x00\x12\x36\n\x07\x66\x61ilure\x18\x03 \x01(\x0b\x32#.chromiumos.test.api.InstallFailureH\x00\x12\x46\n\x10\x61ndroid_packages\x18\x04 \x03(\x0b\x32,.chromiumos.test.api.InstalledAndroidPackageB\t\n\x07outcomeB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3'
+  serialized_pb=b'\n/chromiumos/test/api/android_provision_cli.proto\x12\x13\x63hromiumos.test.api\x1a\x34\x63hromiumos/test/api/android_provision_metadata.proto\x1a+chromiumos/test/api/provision_service.proto\x1a!chromiumos/test/lab/api/dut.proto\"\xd4\x02\n\x1b\x41ndroidProvisionCLIResponse\x12+\n\x02id\x18\x01 \x01(\x0b\x32\x1f.chromiumos.test.lab.api.Dut.Id\x12\x36\n\x07success\x18\x02 \x01(\x0b\x32#.chromiumos.test.api.InstallSuccessH\x00\x12\x36\n\x07\x66\x61ilure\x18\x03 \x01(\x0b\x32#.chromiumos.test.api.InstallFailureH\x00\x12\x46\n\x10\x61ndroid_packages\x18\x04 \x03(\x0b\x32,.chromiumos.test.api.InstalledAndroidPackage\x12\x45\n\x14installed_android_os\x18\x05 \x01(\x0b\x32\'.chromiumos.test.api.InstalledAndroidOSB\t\n\x07outcomeB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3'
   ,
   dependencies=[chromiumos_dot_test_dot_api_dot_android__provision__metadata__pb2.DESCRIPTOR,chromiumos_dot_test_dot_api_dot_provision__service__pb2.DESCRIPTOR,chromiumos_dot_test_dot_lab_dot_api_dot_dut__pb2.DESCRIPTOR,])
 
@@ -65,6 +65,13 @@ _ANDROIDPROVISIONCLIRESPONSE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='installed_android_os', full_name='chromiumos.test.api.AndroidProvisionCLIResponse.installed_android_os', index=4,
+      number=5, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -83,13 +90,14 @@ _ANDROIDPROVISIONCLIRESPONSE = _descriptor.Descriptor(
     fields=[]),
   ],
   serialized_start=207,
-  serialized_end=476,
+  serialized_end=547,
 )
 
 _ANDROIDPROVISIONCLIRESPONSE.fields_by_name['id'].message_type = chromiumos_dot_test_dot_lab_dot_api_dot_dut__pb2._DUT_ID
 _ANDROIDPROVISIONCLIRESPONSE.fields_by_name['success'].message_type = chromiumos_dot_test_dot_api_dot_provision__service__pb2._INSTALLSUCCESS
 _ANDROIDPROVISIONCLIRESPONSE.fields_by_name['failure'].message_type = chromiumos_dot_test_dot_api_dot_provision__service__pb2._INSTALLFAILURE
 _ANDROIDPROVISIONCLIRESPONSE.fields_by_name['android_packages'].message_type = chromiumos_dot_test_dot_api_dot_android__provision__metadata__pb2._INSTALLEDANDROIDPACKAGE
+_ANDROIDPROVISIONCLIRESPONSE.fields_by_name['installed_android_os'].message_type = chromiumos_dot_test_dot_api_dot_android__provision__metadata__pb2._INSTALLEDANDROIDOS
 _ANDROIDPROVISIONCLIRESPONSE.oneofs_by_name['outcome'].fields.append(
   _ANDROIDPROVISIONCLIRESPONSE.fields_by_name['success'])
 _ANDROIDPROVISIONCLIRESPONSE.fields_by_name['success'].containing_oneof = _ANDROIDPROVISIONCLIRESPONSE.oneofs_by_name['outcome']
