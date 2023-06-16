3b21e2638a2dea55b314f7ab70b9a54babe3b9b0
Gwendal Grignou <gwendal@chromium.org>
2023-06-15 17:58:20 -0700

    PRESUBMIT: Check with cros lint
    Lint file, mostly fix indentation to 4 spaces.
    
    BUG=none
    TEST=repo upload.
    
    Change-Id: I0ec29f3b24263035b2b192376fb6d4545943043d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/4621647
    Reviewed-by: Jack Rosenthal <jrosenth@chromium.org>
    Commit-Queue: Gwendal Grignou <gwendal@chromium.org>

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -5,55 +5,72 @@
 # For details on the depot tools provided presubmit API see:
 # http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 
+"""Hook to run unit tests on upload."""
+
 import sys
 
-sys.path.insert(1, 'presubmit')
+
+sys.path.insert(1, "presubmit")
+# pylint: disable=wrong-import-position
 import presubmits
+
+
+# pylint: enable=wrong-import-position
+
 
 USE_PYTHON3 = True
 
+
 def CheckGenerated(input_api, output_api):
-  """Checks all scripts that produce generated output.
+    """Checks all scripts that produce generated output.
 
     Checks that all of the scripts that produce generated output in this
     repository have been ran and that the generated output is up to date.
 
     Args:
-      input_api: InputApi, provides information about the change.
-      output_api: OutputApi, provides the mechanism for returning a response.
+        input_api: InputApi, provides information about the change.
+        output_api: OutputApi, provides the mechanism for returning a response.
 
     Returns:
-      list of PresubmitError, or empty list if no errors.
+        list of PresubmitError, or empty list if no errors.
     """
-  results = []
+    results = []
 
-  # Starting with generate.sh.
-  results.extend(presubmits.CheckGenerated(input_api, output_api))
+    # Starting with generate.sh.
+    results.extend(presubmits.CheckGenerated(input_api, output_api))
 
-  # The generate.sh in this repo can create files. Make sure repo is clean.
-  results.extend(presubmits.CheckUntracked(input_api, output_api))
+    # The generate.sh in this repo can create files. Make sure repo is clean.
+    results.extend(presubmits.CheckUntracked(input_api, output_api))
 
-  return results
+    return results
 
 
 def CommonChecks(input_api, output_api):
-  file_filter = lambda x: x.LocalPath() == 'infra/config/recipes.cfg'
-  results = input_api.canned_checks.CheckJsonParses(
-      input_api, output_api, file_filter=file_filter)
-  results.extend(CheckGenerated(input_api, output_api))
-  for script in [
-      './run_py_unittests.sh',
-      './run_go_unittests.sh',
-      './check_starlark.sh',
-      'vpython3 -vpython-spec .vpython presubmit/check_dut_attributes.py',
-  ]:
-    results.extend(presubmits.CheckScript(input_api, output_api, script))
-  return results
+    """Code to check the generated code and .star files."""
+    file_filter = lambda x: x.LocalPath() == "infra/config/recipes.cfg"
+    results = input_api.canned_checks.CheckJsonParses(
+        input_api, output_api, file_filter=file_filter
+    )
+    results.extend(CheckGenerated(input_api, output_api))
+    for script in [
+        "./run_py_unittests.sh",
+        "./run_go_unittests.sh",
+        "./check_starlark.sh",
+        "vpython3 -vpython-spec .vpython presubmit/check_dut_attributes.py",
+    ]:
+        results.extend(presubmits.CheckScript(input_api, output_api, script))
+    return results
 
 
 def CheckChangeOnUpload(input_api, output_api):
-  return CommonChecks(input_api, output_api)
+    """Upload hook.
+
+    Normally disabled on ChromeOS, but used in config to validate the generated code.
+
+    """
+    return CommonChecks(input_api, output_api)
 
 
 def CheckChangeOnCommit(input_api, output_api):
-  return CommonChecks(input_api, output_api)
+    """Commit hook."""
+    return CommonChecks(input_api, output_api)
