f889736ccea43faf7affb1da2ff5c9afe7442689
Chris DeLaGarza <cdelagarza@google.com>
2023-07-14 16:58:15 +0000

    cft: add generic startup rpc to generic provision service
    In order to be more generic and standard across specific provisions, server starts should only deal with exposing the address and not dealing with any initialization arguments.
    
    Add a route for initializing the provision service..
    
    BUG=b:291286186
    TEST=./generate.sh
    
    Change-Id: I33df429d9db20732c827d4cb58b2da10cb1bb527
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/4685334
    Reviewed-by: Azizur Rahman <azrahman@google.com>
    Auto-Submit: Chris DeLaGarza <cdelagarza@google.com>
    Commit-Queue: Chris DeLaGarza <cdelagarza@google.com>

--- python/chromiumos/test/api/provision_pb2.py
+++ python/chromiumos/test/api/provision_pb2.py
@@ -13,6 +13,8 @@ _sym_db = _symbol_database.Default()
 
 from chromiumos.longrunning import operations_pb2 as chromiumos_dot_longrunning_dot_operations__pb2
 from chromiumos import storage_path_pb2 as chromiumos_dot_storage__path__pb2
+from chromiumos.test.lab.api import dut_pb2 as chromiumos_dot_test_dot_lab_dot_api_dot_dut__pb2
+from chromiumos.test.lab.api import ip_endpoint_pb2 as chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2
 from google.protobuf import any_pb2 as google_dot_protobuf_dot_any__pb2
 
 
@@ -22,11 +24,46 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z-go.chromium.org/chromiumos/config/go/test/api',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n#chromiumos/test/api/provision.proto\x12\x13\x63hromiumos.test.api\x1a\'chromiumos/longrunning/operations.proto\x1a\x1d\x63hromiumos/storage_path.proto\x1a\x19google/protobuf/any.proto\"\x89\x07\n\x0fInstallResponse\x12;\n\x06status\x18\x01 \x01(\x0e\x32+.chromiumos.test.api.InstallResponse.Status\x12&\n\x08metadata\x18\x02 \x01(\x0b\x32\x14.google.protobuf.Any\"\x90\x06\n\x06Status\x12\r\n\tSTATUS_OK\x10\x00\x12\x1a\n\x16STATUS_INVALID_REQUEST\x10\x01\x12(\n$STATUS_DUT_UNREACHABLE_PRE_PROVISION\x10\x02\x12#\n\x1fSTATUS_DOWNLOADING_IMAGE_FAILED\x10\x03\x12 \n\x1cSTATUS_PROVISIONING_TIMEDOUT\x10\x04\x12\x1e\n\x1aSTATUS_PROVISIONING_FAILED\x10\x05\x12)\n%STATUS_DUT_UNREACHABLE_POST_PROVISION\x10\x06\x12!\n\x1dSTATUS_UPDATE_FIRMWARE_FAILED\x10\x07\x12\x31\n-STATUS_FIRMWARE_MISMATCH_POST_FIRMWARE_UPDATE\x10\x08\x12/\n+STATUS_DUT_UNREACHABLE_POST_FIRMWARE_UPDATE\x10\t\x12\x1f\n\x1bSTATUS_UPDATE_MINIOS_FAILED\x10\n\x12&\n\"STATUS_POST_PROVISION_SETUP_FAILED\x10\x0b\x12\x1b\n\x17STATUS_CLEAR_TPM_FAILED\x10\x0c\x12\x1e\n\x1aSTATUS_STABLIZE_DUT_FAILED\x10\r\x12\x1d\n\x19STATUS_INSTALL_DLC_FAILED\x10\x0e\x12%\n!STATUS_PRE_PROVISION_SETUP_FAILED\x10\x0f\x12%\n!STATUS_CIPD_PACKAGE_LOOKUP_FAILED\x10\x10\x12$\n STATUS_CIPD_PACKAGE_FETCH_FAILED\x10\x11\x12\x1b\n\x17STATUS_GS_UPLOAD_FAILED\x10\x12\x12\x1d\n\x19STATUS_GS_DOWNLOAD_FAILED\x10\x13\x12\x12\n\x0eSTATUS_SUCCESS\x10\x14\x12/\n+STATUS_IMAGE_MISMATCH_POST_PROVISION_UPDATE\x10\x15\"\x11\n\x0fInstallMetadata\"\xb1\x01\n\x0eInstallRequest\x12+\n\nimage_path\x18\x01 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12\x16\n\x0eprevent_reboot\x18\x02 \x01(\x08\x12\x32\n\x11overwrite_payload\x18\x03 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12&\n\x08metadata\x18\x04 \x01(\x0b\x32\x14.google.protobuf.Any2\x93\x01\n\x17GenericProvisionService\x12x\n\x07Install\x12#.chromiumos.test.api.InstallRequest\x1a!.chromiumos.longrunning.Operation\"%\xd2\x41\"\n\x0fInstallResponse\x12\x0fInstallMetadataB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3'
+  serialized_pb=b'\n#chromiumos/test/api/provision.proto\x12\x13\x63hromiumos.test.api\x1a\'chromiumos/longrunning/operations.proto\x1a\x1d\x63hromiumos/storage_path.proto\x1a!chromiumos/test/lab/api/dut.proto\x1a)chromiumos/test/lab/api/ip_endpoint.proto\x1a\x19google/protobuf/any.proto\"\xa5\x01\n\x17ProvisionStartupRequest\x12)\n\x03\x64ut\x18\x01 \x01(\x0b\x32\x1c.chromiumos.test.lab.api.Dut\x12\x37\n\ndut_server\x18\x02 \x01(\x0b\x32#.chromiumos.test.lab.api.IpEndpoint\x12&\n\x08metadata\x18\x03 \x01(\x0b\x32\x14.google.protobuf.Any\"\xcd\x01\n\x18ProvisionStartupResponse\x12\x44\n\x06status\x18\x01 \x01(\x0e\x32\x34.chromiumos.test.api.ProvisionStartupResponse.Status\"k\n\x06Status\x12\x16\n\x12STATUS_UNSPECIFIED\x10\x00\x12\x12\n\x0eSTATUS_SUCCESS\x10\x01\x12\x1a\n\x16STATUS_INVALID_REQUEST\x10\x02\x12\x19\n\x15STATUS_STARTUP_FAILED\x10\x03\"\x89\x07\n\x0fInstallResponse\x12;\n\x06status\x18\x01 \x01(\x0e\x32+.chromiumos.test.api.InstallResponse.Status\x12&\n\x08metadata\x18\x02 \x01(\x0b\x32\x14.google.protobuf.Any\"\x90\x06\n\x06Status\x12\r\n\tSTATUS_OK\x10\x00\x12\x1a\n\x16STATUS_INVALID_REQUEST\x10\x01\x12(\n$STATUS_DUT_UNREACHABLE_PRE_PROVISION\x10\x02\x12#\n\x1fSTATUS_DOWNLOADING_IMAGE_FAILED\x10\x03\x12 \n\x1cSTATUS_PROVISIONING_TIMEDOUT\x10\x04\x12\x1e\n\x1aSTATUS_PROVISIONING_FAILED\x10\x05\x12)\n%STATUS_DUT_UNREACHABLE_POST_PROVISION\x10\x06\x12!\n\x1dSTATUS_UPDATE_FIRMWARE_FAILED\x10\x07\x12\x31\n-STATUS_FIRMWARE_MISMATCH_POST_FIRMWARE_UPDATE\x10\x08\x12/\n+STATUS_DUT_UNREACHABLE_POST_FIRMWARE_UPDATE\x10\t\x12\x1f\n\x1bSTATUS_UPDATE_MINIOS_FAILED\x10\n\x12&\n\"STATUS_POST_PROVISION_SETUP_FAILED\x10\x0b\x12\x1b\n\x17STATUS_CLEAR_TPM_FAILED\x10\x0c\x12\x1e\n\x1aSTATUS_STABLIZE_DUT_FAILED\x10\r\x12\x1d\n\x19STATUS_INSTALL_DLC_FAILED\x10\x0e\x12%\n!STATUS_PRE_PROVISION_SETUP_FAILED\x10\x0f\x12%\n!STATUS_CIPD_PACKAGE_LOOKUP_FAILED\x10\x10\x12$\n STATUS_CIPD_PACKAGE_FETCH_FAILED\x10\x11\x12\x1b\n\x17STATUS_GS_UPLOAD_FAILED\x10\x12\x12\x1d\n\x19STATUS_GS_DOWNLOAD_FAILED\x10\x13\x12\x12\n\x0eSTATUS_SUCCESS\x10\x14\x12/\n+STATUS_IMAGE_MISMATCH_POST_PROVISION_UPDATE\x10\x15\"\x11\n\x0fInstallMetadata\"\xb1\x01\n\x0eInstallRequest\x12+\n\nimage_path\x18\x01 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12\x16\n\x0eprevent_reboot\x18\x02 \x01(\x08\x12\x32\n\x11overwrite_payload\x18\x03 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12&\n\x08metadata\x18\x04 \x01(\x0b\x32\x14.google.protobuf.Any2\x93\x01\n\x17GenericProvisionService\x12x\n\x07Install\x12#.chromiumos.test.api.InstallRequest\x1a!.chromiumos.longrunning.Operation\"%\xd2\x41\"\n\x0fInstallResponse\x12\x0fInstallMetadataB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3'
   ,
-  dependencies=[chromiumos_dot_longrunning_dot_operations__pb2.DESCRIPTOR,chromiumos_dot_storage__path__pb2.DESCRIPTOR,google_dot_protobuf_dot_any__pb2.DESCRIPTOR,])
+  dependencies=[chromiumos_dot_longrunning_dot_operations__pb2.DESCRIPTOR,chromiumos_dot_storage__path__pb2.DESCRIPTOR,chromiumos_dot_test_dot_lab_dot_api_dot_dut__pb2.DESCRIPTOR,chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2.DESCRIPTOR,google_dot_protobuf_dot_any__pb2.DESCRIPTOR,])
 
 
+
+_PROVISIONSTARTUPRESPONSE_STATUS = _descriptor.EnumDescriptor(
+  name='Status',
+  full_name='chromiumos.test.api.ProvisionStartupResponse.Status',
+  filename=None,
+  file=DESCRIPTOR,
+  create_key=_descriptor._internal_create_key,
+  values=[
+    _descriptor.EnumValueDescriptor(
+      name='STATUS_UNSPECIFIED', index=0, number=0,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='STATUS_SUCCESS', index=1, number=1,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='STATUS_INVALID_REQUEST', index=2, number=2,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='STATUS_STARTUP_FAILED', index=3, number=3,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
+  ],
+  containing_type=None,
+  serialized_options=None,
+  serialized_start=504,
+  serialized_end=611,
+)
+_sym_db.RegisterEnumDescriptor(_PROVISIONSTARTUPRESPONSE_STATUS)
 
 _INSTALLRESPONSE_STATUS = _descriptor.EnumDescriptor(
   name='Status',
@@ -148,10 +185,89 @@ _INSTALLRESPONSE_STATUS = _descriptor.EnumDescriptor(
   ],
   containing_type=None,
   serialized_options=None,
-  serialized_start=281,
-  serialized_end=1065,
+  serialized_start=735,
+  serialized_end=1519,
 )
 _sym_db.RegisterEnumDescriptor(_INSTALLRESPONSE_STATUS)
+
+
+_PROVISIONSTARTUPREQUEST = _descriptor.Descriptor(
+  name='ProvisionStartupRequest',
+  full_name='chromiumos.test.api.ProvisionStartupRequest',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='dut', full_name='chromiumos.test.api.ProvisionStartupRequest.dut', index=0,
+      number=1, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='dut_server', full_name='chromiumos.test.api.ProvisionStartupRequest.dut_server', index=1,
+      number=2, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='metadata', full_name='chromiumos.test.api.ProvisionStartupRequest.metadata', index=2,
+      number=3, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=238,
+  serialized_end=403,
+)
+
+
+_PROVISIONSTARTUPRESPONSE = _descriptor.Descriptor(
+  name='ProvisionStartupResponse',
+  full_name='chromiumos.test.api.ProvisionStartupResponse',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='status', full_name='chromiumos.test.api.ProvisionStartupResponse.status', index=0,
+      number=1, type=14, cpp_type=8, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+    _PROVISIONSTARTUPRESPONSE_STATUS,
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=406,
+  serialized_end=611,
+)
 
 
 _INSTALLRESPONSE = _descriptor.Descriptor(
@@ -189,8 +305,8 @@ _INSTALLRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=160,
-  serialized_end=1065,
+  serialized_start=614,
+  serialized_end=1519,
 )
 
 
@@ -214,8 +330,8 @@ _INSTALLMETADATA = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1067,
-  serialized_end=1084,
+  serialized_start=1521,
+  serialized_end=1538,
 )
 
 
@@ -267,20 +383,41 @@ _INSTALLREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1087,
-  serialized_end=1264,
+  serialized_start=1541,
+  serialized_end=1718,
 )
 
+_PROVISIONSTARTUPREQUEST.fields_by_name['dut'].message_type = chromiumos_dot_test_dot_lab_dot_api_dot_dut__pb2._DUT
+_PROVISIONSTARTUPREQUEST.fields_by_name['dut_server'].message_type = chromiumos_dot_test_dot_lab_dot_api_dot_ip__endpoint__pb2._IPENDPOINT
+_PROVISIONSTARTUPREQUEST.fields_by_name['metadata'].message_type = google_dot_protobuf_dot_any__pb2._ANY
+_PROVISIONSTARTUPRESPONSE.fields_by_name['status'].enum_type = _PROVISIONSTARTUPRESPONSE_STATUS
+_PROVISIONSTARTUPRESPONSE_STATUS.containing_type = _PROVISIONSTARTUPRESPONSE
 _INSTALLRESPONSE.fields_by_name['status'].enum_type = _INSTALLRESPONSE_STATUS
 _INSTALLRESPONSE.fields_by_name['metadata'].message_type = google_dot_protobuf_dot_any__pb2._ANY
 _INSTALLRESPONSE_STATUS.containing_type = _INSTALLRESPONSE
 _INSTALLREQUEST.fields_by_name['image_path'].message_type = chromiumos_dot_storage__path__pb2._STORAGEPATH
 _INSTALLREQUEST.fields_by_name['overwrite_payload'].message_type = chromiumos_dot_storage__path__pb2._STORAGEPATH
 _INSTALLREQUEST.fields_by_name['metadata'].message_type = google_dot_protobuf_dot_any__pb2._ANY
+DESCRIPTOR.message_types_by_name['ProvisionStartupRequest'] = _PROVISIONSTARTUPREQUEST
+DESCRIPTOR.message_types_by_name['ProvisionStartupResponse'] = _PROVISIONSTARTUPRESPONSE
 DESCRIPTOR.message_types_by_name['InstallResponse'] = _INSTALLRESPONSE
 DESCRIPTOR.message_types_by_name['InstallMetadata'] = _INSTALLMETADATA
 DESCRIPTOR.message_types_by_name['InstallRequest'] = _INSTALLREQUEST
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
+
+ProvisionStartupRequest = _reflection.GeneratedProtocolMessageType('ProvisionStartupRequest', (_message.Message,), {
+  'DESCRIPTOR' : _PROVISIONSTARTUPREQUEST,
+  '__module__' : 'chromiumos.test.api.provision_pb2'
+  # @@protoc_insertion_point(class_scope:chromiumos.test.api.ProvisionStartupRequest)
+  })
+_sym_db.RegisterMessage(ProvisionStartupRequest)
+
+ProvisionStartupResponse = _reflection.GeneratedProtocolMessageType('ProvisionStartupResponse', (_message.Message,), {
+  'DESCRIPTOR' : _PROVISIONSTARTUPRESPONSE,
+  '__module__' : 'chromiumos.test.api.provision_pb2'
+  # @@protoc_insertion_point(class_scope:chromiumos.test.api.ProvisionStartupResponse)
+  })
+_sym_db.RegisterMessage(ProvisionStartupResponse)
 
 InstallResponse = _reflection.GeneratedProtocolMessageType('InstallResponse', (_message.Message,), {
   'DESCRIPTOR' : _INSTALLRESPONSE,
@@ -313,8 +450,8 @@ _GENERICPROVISIONSERVICE = _descriptor.ServiceDescriptor(
   index=0,
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_start=1267,
-  serialized_end=1414,
+  serialized_start=1721,
+  serialized_end=1868,
   methods=[
   _descriptor.MethodDescriptor(
     name='Install',
