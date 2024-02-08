c47131b3c59081a4eda0f3717e1633e18958b9d7
Jard Loucks <jaredloucks@google.com>
2024-02-08 09:45:10 -0700

    scheduke: add "canceled" task state
    Change-Id: I84c1a98135e7e33a027d4ffc96bc8cd2f69aa41e
    Bug: b:304830236
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/5279401
    Auto-Submit: Jard Loucks <jaredloucks@google.com>
    Reviewed-by: Derek Beckett <dbeckett@chromium.org>
    Commit-Queue: Derek Beckett <dbeckett@chromium.org>

--- python/chromiumos/test/scheduling/task_state_pb2.py
+++ python/chromiumos/test/scheduling/task_state_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z\007./proto',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n+chromiumos/test/scheduling/task_state.proto\x12\x1a\x63hromiumos.test.scheduling\"\x92\x01\n\x18\x43reateTaskStatesResponse\x12J\n\x03ids\x18\x01 \x03(\x0b\x32=.chromiumos.test.scheduling.CreateTaskStatesResponse.IdsEntry\x1a*\n\x08IdsEntry\x12\x0b\n\x03key\x18\x01 \x01(\x03\x12\r\n\x05value\x18\x02 \x01(\x03:\x02\x38\x01\"R\n\x16ReadTaskStatesResponse\x12\x38\n\x05tasks\x18\x01 \x03(\x0b\x32).chromiumos.test.scheduling.TaskWithState\"j\n\rTaskWithState\x12\x15\n\rtask_state_id\x18\x01 \x01(\x03\x12\x34\n\x05state\x18\x02 \x01(\x0e\x32%.chromiumos.test.scheduling.TaskState\x12\x0c\n\x04\x62\x62id\x18\x03 \x01(\x03*Q\n\tTaskState\x12\r\n\tREQUESTED\x10\x00\x12\x0b\n\x07PENDING\x10\x01\x12\x0c\n\x08LAUNCHED\x10\x02\x12\r\n\tCOMPLETED\x10\x03\x12\x0b\n\x07\x45XPIRED\x10\x04\x42\tZ\x07./protob\x06proto3'
+  serialized_pb=b'\n+chromiumos/test/scheduling/task_state.proto\x12\x1a\x63hromiumos.test.scheduling\"\x92\x01\n\x18\x43reateTaskStatesResponse\x12J\n\x03ids\x18\x01 \x03(\x0b\x32=.chromiumos.test.scheduling.CreateTaskStatesResponse.IdsEntry\x1a*\n\x08IdsEntry\x12\x0b\n\x03key\x18\x01 \x01(\x03\x12\r\n\x05value\x18\x02 \x01(\x03:\x02\x38\x01\"R\n\x16ReadTaskStatesResponse\x12\x38\n\x05tasks\x18\x01 \x03(\x0b\x32).chromiumos.test.scheduling.TaskWithState\"j\n\rTaskWithState\x12\x15\n\rtask_state_id\x18\x01 \x01(\x03\x12\x34\n\x05state\x18\x02 \x01(\x0e\x32%.chromiumos.test.scheduling.TaskState\x12\x0c\n\x04\x62\x62id\x18\x03 \x01(\x03*_\n\tTaskState\x12\r\n\tREQUESTED\x10\x00\x12\x0b\n\x07PENDING\x10\x01\x12\x0c\n\x08LAUNCHED\x10\x02\x12\r\n\tCOMPLETED\x10\x03\x12\x0b\n\x07\x45XPIRED\x10\x04\x12\x0c\n\x08\x43\x41NCELED\x10\x05\x42\tZ\x07./protob\x06proto3'
 )
 
 _TASKSTATE = _descriptor.EnumDescriptor(
@@ -55,11 +55,16 @@ _TASKSTATE = _descriptor.EnumDescriptor(
       serialized_options=None,
       type=None,
       create_key=_descriptor._internal_create_key),
+    _descriptor.EnumValueDescriptor(
+      name='CANCELED', index=5, number=5,
+      serialized_options=None,
+      type=None,
+      create_key=_descriptor._internal_create_key),
   ],
   containing_type=None,
   serialized_options=None,
   serialized_start=416,
-  serialized_end=497,
+  serialized_end=511,
 )
 _sym_db.RegisterEnumDescriptor(_TASKSTATE)
 
@@ -69,6 +74,7 @@ PENDING = 1
 LAUNCHED = 2
 COMPLETED = 3
 EXPIRED = 4
+CANCELED = 5
 
 
 
