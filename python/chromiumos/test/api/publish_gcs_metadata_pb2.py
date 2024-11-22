dd64baa50499dbdc03128c3fcb3a97dc31212ece
Amir Mousavi <amirmousavi@google.com>
2024-11-22 23:22:58 +0000

    gcs_publish proto: Add bool field to the request to enable xts archiver.
    Test: `./generate.sh`
    Bug: b/379711782
    Change-Id: Ib30a8987c4b0ca801a1f0488b447ee8a3febae53
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/6045022
    Commit-Queue: Amir Mousavi <amirmousavi@google.com>
    Reviewed-by: Azizur Rahman <azrahman@google.com>
    Reviewed-by: Jason Kusuma <jkusuma@google.com>

--- python/chromiumos/test/api/publish_gcs_metadata_pb2.py
+++ python/chromiumos/test/api/publish_gcs_metadata_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z-go.chromium.org/chromiumos/config/go/test/api',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n.chromiumos/test/api/publish_gcs_metadata.proto\x12\x13\x63hromiumos.test.api\x1a\x1d\x63hromiumos/storage_path.proto\"\x9b\x01\n\x13XtsArchiverMetadata\x12\x0e\n\x06\x61l_run\x18\x01 \x01(\x08\x12\x0f\n\x07product\x18\x02 \x01(\t\x12\r\n\x05\x62uild\x18\x03 \x01(\t\x12\x1f\n\x17parent_swarming_task_id\x18\x04 \x01(\t\x12\x1a\n\x12results_gcs_prefix\x18\x05 \x01(\t\x12\x17\n\x0f\x61pfe_gcs_prefix\x18\x06 \x01(\t\"\xca\x01\n\x12PublishGcsMetadata\x12)\n\x08gcs_path\x18\x01 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12@\n\x1fservice_account_creds_file_path\x18\x02 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12G\n\x15xts_archiver_metadata\x18\x03 \x01(\x0b\x32(.chromiumos.test.api.XtsArchiverMetadataB/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3'
+  serialized_pb=b'\n.chromiumos/test/api/publish_gcs_metadata.proto\x12\x13\x63hromiumos.test.api\x1a\x1d\x63hromiumos/storage_path.proto\"\x9b\x01\n\x13XtsArchiverMetadata\x12\x0e\n\x06\x61l_run\x18\x01 \x01(\x08\x12\x0f\n\x07product\x18\x02 \x01(\t\x12\r\n\x05\x62uild\x18\x03 \x01(\t\x12\x1f\n\x17parent_swarming_task_id\x18\x04 \x01(\t\x12\x1a\n\x12results_gcs_prefix\x18\x05 \x01(\t\x12\x17\n\x0f\x61pfe_gcs_prefix\x18\x06 \x01(\t\"\xe7\x01\n\x12PublishGcsMetadata\x12)\n\x08gcs_path\x18\x01 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12@\n\x1fservice_account_creds_file_path\x18\x02 \x01(\x0b\x32\x17.chromiumos.StoragePath\x12G\n\x15xts_archiver_metadata\x18\x03 \x01(\x0b\x32(.chromiumos.test.api.XtsArchiverMetadata\x12\x1b\n\x13\x65nable_xts_archiver\x18\x04 \x01(\x08\x42/Z-go.chromium.org/chromiumos/config/go/test/apib\x06proto3'
   ,
   dependencies=[chromiumos_dot_storage__path__pb2.DESCRIPTOR,])
 
@@ -123,6 +123,13 @@ _PUBLISHGCSMETADATA = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='enable_xts_archiver', full_name='chromiumos.test.api.PublishGcsMetadata.enable_xts_archiver', index=3,
+      number=4, type=8, cpp_type=7, label=1,
+      has_default_value=False, default_value=False,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -136,7 +143,7 @@ _PUBLISHGCSMETADATA = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=261,
-  serialized_end=463,
+  serialized_end=492,
 )
 
 _PUBLISHGCSMETADATA.fields_by_name['gcs_path'].message_type = chromiumos_dot_storage__path__pb2._STORAGEPATH
