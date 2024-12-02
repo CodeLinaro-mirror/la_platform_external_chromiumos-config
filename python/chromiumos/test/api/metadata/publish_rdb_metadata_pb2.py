925e08dc46f7e3f2b4f69e2f9d13f6e9746825b1
Allen Xie <zhihuixie@google.com>
2024-12-02 18:08:17 +1100

    rdb_publish_proto: add the response of the firmware provision step
    Bug: b:330897562
    Test: Unit test
    Change-Id: I416beb2d16a04bfe211569e3ab5127850eba7e44
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/6060360
    Commit-Queue: Chris DeLaGarza <cdelagarza@google.com>
    Reviewed-by: Jason Kusuma <jkusuma@google.com>
    Reviewed-by: Chris DeLaGarza <cdelagarza@google.com>

--- python/chromiumos/test/api/metadata/publish_rdb_metadata_pb2.py
+++ python/chromiumos/test/api/metadata/publish_rdb_metadata_pb2.py
@@ -12,6 +12,7 @@ _sym_db = _symbol_database.Default()
 
 
 from chromiumos.test.artifact import test_result_pb2 as chromiumos_dot_test_dot_artifact_dot_test__result__pb2
+from chromiumos.test.api import firmware_provision_pb2 as chromiumos_dot_test_dot_api_dot_firmware__provision__pb2
 from chromiumos.test.api import post_test_service_pb2 as chromiumos_dot_test_dot_api_dot_post__test__service__pb2
 
 
@@ -21,9 +22,9 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=b'Z6go.chromium.org/chromiumos/config/go/test/api/metadata',
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n7chromiumos/test/api/metadata/publish_rdb_metadata.proto\x12\x1c\x63hromiumos.test.api.metadata\x1a*chromiumos/test/artifact/test_result.proto\x1a+chromiumos/test/api/post_test_service.proto\"\xfc\x03\n\x12PublishRdbMetadata\x12\x1d\n\x15\x63urrent_invocation_id\x18\x01 \x01(\t\x12\x39\n\x0btest_result\x18\x02 \x01(\x0b\x32$.chromiumos.test.artifact.TestResult\x12\x19\n\rstainless_url\x18\x03 \x01(\tB\x02\x18\x01\x12\x14\n\x0ctesthaus_url\x18\x04 \x01(\t\x12I\n\x07sources\x18\x05 \x01(\x0b\x32\x38.chromiumos.test.api.metadata.PublishRdbMetadata.Sources\x12W\n\x0c\x62\x61se_variant\x18\x06 \x03(\x0b\x32\x41.chromiumos.test.api.metadata.PublishRdbMetadata.BaseVariantEntry\x12J\n\x16post_process_responses\x18\x07 \x01(\x0b\x32*.chromiumos.test.api.RunActivitiesResponse\x1a\x37\n\x07Sources\x12\x0f\n\x07gs_path\x18\x01 \x01(\t\x12\x1b\n\x13is_deployment_dirty\x18\x02 \x01(\x08\x1a\x32\n\x10\x42\x61seVariantEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x38Z6go.chromium.org/chromiumos/config/go/test/api/metadatab\x06proto3'
+  serialized_pb=b'\n7chromiumos/test/api/metadata/publish_rdb_metadata.proto\x12\x1c\x63hromiumos.test.api.metadata\x1a*chromiumos/test/artifact/test_result.proto\x1a,chromiumos/test/api/firmware_provision.proto\x1a+chromiumos/test/api/post_test_service.proto\"\xd1\x04\n\x12PublishRdbMetadata\x12\x1d\n\x15\x63urrent_invocation_id\x18\x01 \x01(\t\x12\x39\n\x0btest_result\x18\x02 \x01(\x0b\x32$.chromiumos.test.artifact.TestResult\x12\x19\n\rstainless_url\x18\x03 \x01(\tB\x02\x18\x01\x12\x14\n\x0ctesthaus_url\x18\x04 \x01(\t\x12I\n\x07sources\x18\x05 \x01(\x0b\x32\x38.chromiumos.test.api.metadata.PublishRdbMetadata.Sources\x12W\n\x0c\x62\x61se_variant\x18\x06 \x03(\x0b\x32\x41.chromiumos.test.api.metadata.PublishRdbMetadata.BaseVariantEntry\x12J\n\x16post_process_responses\x18\x07 \x01(\x0b\x32*.chromiumos.test.api.RunActivitiesResponse\x12S\n\x1b\x66irmware_provision_response\x18\x08 \x01(\x0b\x32..chromiumos.test.api.FirmwareProvisionResponse\x1a\x37\n\x07Sources\x12\x0f\n\x07gs_path\x18\x01 \x01(\t\x12\x1b\n\x13is_deployment_dirty\x18\x02 \x01(\x08\x1a\x32\n\x10\x42\x61seVariantEntry\x12\x0b\n\x03key\x18\x01 \x01(\t\x12\r\n\x05value\x18\x02 \x01(\t:\x02\x38\x01\x42\x38Z6go.chromium.org/chromiumos/config/go/test/api/metadatab\x06proto3'
   ,
-  dependencies=[chromiumos_dot_test_dot_artifact_dot_test__result__pb2.DESCRIPTOR,chromiumos_dot_test_dot_api_dot_post__test__service__pb2.DESCRIPTOR,])
+  dependencies=[chromiumos_dot_test_dot_artifact_dot_test__result__pb2.DESCRIPTOR,chromiumos_dot_test_dot_api_dot_firmware__provision__pb2.DESCRIPTOR,chromiumos_dot_test_dot_api_dot_post__test__service__pb2.DESCRIPTOR,])
 
 
 
@@ -62,8 +63,8 @@ _PUBLISHRDBMETADATA_SOURCES = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=580,
-  serialized_end=635,
+  serialized_start=711,
+  serialized_end=766,
 )
 
 _PUBLISHRDBMETADATA_BASEVARIANTENTRY = _descriptor.Descriptor(
@@ -100,8 +101,8 @@ _PUBLISHRDBMETADATA_BASEVARIANTENTRY = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=637,
-  serialized_end=687,
+  serialized_start=768,
+  serialized_end=818,
 )
 
 _PUBLISHRDBMETADATA = _descriptor.Descriptor(
@@ -161,6 +162,13 @@ _PUBLISHRDBMETADATA = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='firmware_provision_response', full_name='chromiumos.test.api.metadata.PublishRdbMetadata.firmware_provision_response', index=7,
+      number=8, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -173,8 +181,8 @@ _PUBLISHRDBMETADATA = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=179,
-  serialized_end=687,
+  serialized_start=225,
+  serialized_end=818,
 )
 
 _PUBLISHRDBMETADATA_SOURCES.containing_type = _PUBLISHRDBMETADATA
@@ -183,6 +191,7 @@ _PUBLISHRDBMETADATA.fields_by_name['test_result'].message_type = chromiumos_dot_
 _PUBLISHRDBMETADATA.fields_by_name['sources'].message_type = _PUBLISHRDBMETADATA_SOURCES
 _PUBLISHRDBMETADATA.fields_by_name['base_variant'].message_type = _PUBLISHRDBMETADATA_BASEVARIANTENTRY
 _PUBLISHRDBMETADATA.fields_by_name['post_process_responses'].message_type = chromiumos_dot_test_dot_api_dot_post__test__service__pb2._RUNACTIVITIESRESPONSE
+_PUBLISHRDBMETADATA.fields_by_name['firmware_provision_response'].message_type = chromiumos_dot_test_dot_api_dot_firmware__provision__pb2._FIRMWAREPROVISIONRESPONSE
 DESCRIPTOR.message_types_by_name['PublishRdbMetadata'] = _PUBLISHRDBMETADATA
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
