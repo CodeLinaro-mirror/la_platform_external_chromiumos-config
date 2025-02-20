da1b077406d320c6063d9069473bb1bb42ee4770
Mike Frysinger <vapier@chromium.org>
2025-02-20 15:30:12 -0500

    run_py_unittests: switch from venv to vpython3 directly
    Rather than go digging into vpython3 internals (for how it's cached
    the local copy of python and wheels), we can invoke vpython3 directly
    to get the same practical result.  This makes it easier for devs to
    reproduce specific commands too as they can copy & paste vpython3
    invocations without needing to enter the venv in their active shell.
    
    BUG=None
    TEST=`./run_py_unittests.sh` passes
    
    Change-Id: I609ff8a514336250da6932f552535a2360056e5b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/6288366
    Commit-Queue: Alex Klein <saklein@chromium.org>
    Auto-Submit: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Alex Klein <saklein@chromium.org>

--- run_py_unittests.sh
+++ run_py_unittests.sh
@@ -6,8 +6,6 @@
 #
 # Runs python unittests in a venv.
 
-readonly bin_dir="$(dirname "$(realpath -e "${BASH_SOURCE[0]}")")"/bin
-
 # Move to this script's directory.
 cd "$(dirname "$0")"
 
@@ -15,24 +13,16 @@ cd "$(dirname "$0")"
 echo "Generating proto bindings..."
 ./generate.sh
 
-# Create and activate venv.
-echo "Creating and activating venv..."
-source "${bin_dir}/common.sh"
-create_venv
-
 # Discover and run unittests in payload_utils.
 echo "Running unittests..."
-python3 -m unittest discover -s payload_utils -p "*test.py"
+vpython3 -m unittest discover -s payload_utils -p "*test.py"
 
 echo "Running pylint..."
-PYTHONPATH=payload_utils pylint "$(pwd)/payload_utils" \
+PYTHONPATH=payload_utils vpython3 -m pylint "$(pwd)/payload_utils" \
     --rcfile=payload_utils/pylintrc
 
 echo "Checking Python files formatted with yapf..."
-if ! yapf --style .style.yapf --diff -r payload_utils ; then
+if ! vpython3 -m yapf --style .style.yapf --diff -r payload_utils ; then
     echo "Python files require reformatting. Please run 'yapf --style .style.yapf --in-place -r payload_utils'."
     exit 1
 fi
-
-# Deactivate venv.
-deactivate
