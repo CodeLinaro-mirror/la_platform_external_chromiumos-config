64d28f5a6a584db9943546c2449f6295e1310368
Mike Frysinger <vapier@chromium.org>
2025-02-20 15:33:44 -0500

    generate_grpc_py_bindings.sh: switch from `find|xargs` to `find -exec`
    This handles quoting properly and avoids executing another pipeline.
    
    BUG=None
    TEST=`./generate_grpc_py_bindings.sh` still works
    
    Change-Id: Ieb28d59f50c50de2af11dcdfa6640f9cd3b99319
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/config/+/6288367
    Auto-Submit: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Raul Rangel <rrangel@chromium.org>
    Commit-Queue: Raul Rangel <rrangel@chromium.org>

--- generate_grpc_py_bindings.sh
+++ generate_grpc_py_bindings.sh
@@ -14,10 +14,11 @@ source "${script_dir}/bin/common.sh"
 create_venv
 
 echo "Generating proto and grpc bindings"
-find proto/ -type f -name '*_service.proto' | xargs \
+find proto/ -type f -name '*_service.proto' -exec \
 python3 -m grpc_tools.protoc \
     -Iproto \
     --python_out=python \
-    --grpc_python_out=python
+    --grpc_python_out=python \
+    {} +
 
 deactivate
